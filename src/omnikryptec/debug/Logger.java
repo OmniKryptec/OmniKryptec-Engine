d69e23f97ce4f2a277f7ae89f80cbe989bdf5f29
pcfreak9000 <roman.borris@gmail.com>
2017-05-10 21:19:59 +0200

    logger updated

--- src/omnikryptec/debug/Logger.java
+++ src/omnikryptec/debug/Logger.java
@@ -7,4 +7,22 @@ public class Logger {
 	private static final PrintStream OLD_SYSOUT = System.out;
 	private static final PrintStream OLD_SYSERR = System.err;
 	
+	private static boolean enabled=false;
+	
+	
+	public static void enabledLoggerRedirection(boolean b){
+		if(b&&!enabled){
+			
+		}else if(!b&&enabled){
+			System.setOut(OLD_SYSOUT);
+			System.setErr(OLD_SYSERR);
+		}
+	}
+	
+	public static enum ErrorLevel{
+		FINEST,FINER,FINE,INFO,WARNING,ERROR;
+	}
+	
+	
+	
 }
