9ab989a0fcb69d86704b1aac7b85093302db89cd
Paul Hagedorn <panzer1119@gmail.com>
2017-05-20 15:23:50 +0200

    Formatted the scene

--- src/omnikryptec/main/Scene.java
+++ src/omnikryptec/main/Scene.java
@@ -13,71 +13,74 @@ import omnikryptec.storing.Camera;
 
 public class Scene {
 
-	private Map<String, RenderChunk> scene = new HashMap<>();
-	private Camera cam;
-	private long cox,coy,coz;
-	
-	public Scene(Camera cam){
-		this.cam = cam;
-	}
-	
-	private String tmp;
-	
-	public void addGameObject(GameObject g) {
-		if(g!=null){
-			if(g instanceof Camera && Logger.isDebugMode()){
-				Logger.log("A Camera should not be added as GameObject!", LogLevel.WARNING);
-				return;
-			}
-			tmp = xyzToString(g.getChunkX(), g.getChunkY(), g.getChunkZ());
-			if (!scene.containsKey(tmp)) {
-				scene.put(tmp, new RenderChunk(g.getChunkX(), g.getChunkY(), g.getChunkZ(), this));
-			}
-			scene.get(tmp).addGameObject(g);
-		}
-	}
+    private final Map<String, RenderChunk> scene = new HashMap<>();
+    private Camera cam;
+    private long cox, coy, coz;
 
-	public GameObject removeGameObject(GameObject g) {
-		if (g != null) {
-			if (g.getMyChunk() != null) {
-				g.getMyChunk().removeGameObject(g);
-			} else {
-				tmp = xyzToString(g.getChunkX(), g.getChunkY(), g.getChunkZ());
-				scene.get(tmp).removeGameObject(g);
-			}
-		}
-		return g;
-	}
+    public Scene(Camera cam){
+        this.cam = cam;
+    }
 
-	private static String xyzToString(long x, long y, long z) {
-		return x + ":" + y + ":" + z;
-	}
+    private String tmp;
 
-	
-	public Camera getCamera(){
-		return cam;
-	}
-	
-	public void setCamera(Camera cam){
-		this.cam = cam;
-	}
-	
-	private long cx,cy,cz;
-	private RenderChunk tmpc;
-	public void frame(IRenderer re, Render info){
-		cx = cam.getChunkX();
-		cy = cam.getChunkY();
-		cz = cam.getChunkZ();
-		for(long x=-cox+cx; x<=cox+cx; x++){
-			for(long y=-coy+cy; y<=coy+cy; y++){
-				for(long z=-coz+cz; z<=coz+cz; z++){
-					if((tmpc = scene.get(xyzToString(x, y, z)))!=null){
-						tmpc.frame(re, info);
-					}
-				}
-			}
-		}
-	}
+    public boolean addGameObject(GameObject g) {
+        if(g != null){
+            if(g instanceof Camera && Logger.isDebugMode()){
+                Logger.log("A Camera should not be added as a GameObject!", LogLevel.WARNING);
+                return false;
+            }
+            tmp = xyzToString(g.getChunkX(), g.getChunkY(), g.getChunkZ());
+            if(!scene.containsKey(tmp)) {
+                scene.put(tmp, new RenderChunk(g.getChunkX(), g.getChunkY(), g.getChunkZ(), this));
+            }
+            scene.get(tmp).addGameObject(g);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	
+    public GameObject removeGameObject(GameObject g) {
+        if(g != null) {
+            if(g.getMyChunk() != null) {
+                g.getMyChunk().removeGameObject(g);
+            } else {
+                tmp = xyzToString(g.getChunkX(), g.getChunkY(), g.getChunkZ());
+                scene.get(tmp).removeGameObject(g);
+            }
+        }
+        return g;
+    }
+
+    private static String xyzToString(long x, long y, long z) {
+        return x + ":" + y + ":" + z;
+    }
+
+
+    public Camera getCamera(){
+        return cam;
+    }
+
+    public void setCamera(Camera cam){
+        this.cam = cam;
+    }
+
+    private long cx, cy, cz;
+    private RenderChunk tmpc;
+
+    public void frame(IRenderer re, Render info) {
+        cx = cam.getChunkX();
+        cy = cam.getChunkY();
+        cz = cam.getChunkZ();
+        for(long x = -cox + cx; x <= cox + cx; x++){
+            for(long y = -coy + cy; y <= coy + cy; y++){
+                for(long z = -coz + cz; z <= coz + cz; z++){
+                    if((tmpc = scene.get(xyzToString(x, y, z))) != null){
+                            tmpc.frame(re, info);
+                    }
+                }
+            }
+        }
+    }
+
 }
