574a2bd15f490a57331241b0da696d6557e08446
Paul Hagedorn <panzer1119@gmail.com>
2017-05-22 20:35:32 +0200

    Added a natives unload method

--- src/omnikryptec/util/NativesLoader.java
+++ src/omnikryptec/util/NativesLoader.java
@@ -12,21 +12,22 @@ public class NativesLoader {
     
     private static final String NATIVESPATH = "/omnikryptec/natives/";
     private static final String LWJGLLIBRARYPATH = "org.lwjgl.librarypath";
-    private static File nativesFolder = null;
+    private static final File NATIVESFOLDER = getStandardNativesFolder();
+    
+    private static boolean nativesLoaded = false;
     
     public static final boolean loadNatives() {
-        final File nativesFolder = getStandardNativesFolder();
-        if(nativesFolder == null) {
+        if(NATIVESFOLDER == null) {
             Logger.log("Loaded natives not successfully, because there is some error with the standard natives folder", LogLevel.WARNING);
             return false;
         }
-        nativesFolder.mkdirs();
-        if(nativesFolder.exists() && nativesFolder.isDirectory()) {
-            boolean good = extractNatives(nativesFolder);
+        NATIVESFOLDER.mkdirs();
+        if(NATIVESFOLDER.exists() && NATIVESFOLDER.isDirectory()) {
+            boolean good = extractNatives(NATIVESFOLDER);
             if(good) {
-                NativesLoader.nativesFolder = nativesFolder;
                 Logger.log("Loaded natives successfully", LogLevel.FINE);
                 registerNatives();
+                nativesLoaded = true;
                 return true;
             } else {
                 Logger.log("Loaded natives not successfully, because something with loading the natives went wrong", LogLevel.WARNING);
@@ -38,8 +39,41 @@ public class NativesLoader {
         }
     }
     
+    public static final boolean unloadNatives() {
+        if(!nativesLoaded) {
+            return false;
+        }
+        try {
+            if(!NATIVESFOLDER.isDirectory()) {
+                Logger.log("Unloaded natives not successfully, because there is some error with the standard natives folder", LogLevel.WARNING);
+                return false;
+            }
+            boolean allGood = true;
+            for(File file : NATIVESFOLDER.listFiles()) {
+                if(!file.delete()) {
+                    file.deleteOnExit();
+                    allGood = false;
+                }
+            }
+            if(allGood) {
+                nativesLoaded = false;
+                return true;
+            } else {
+                Logger.log("Unloaded natives not successfully, because some files could not be deleted", LogLevel.WARNING);
+                return false;
+            }
+        } catch (Exception ex) {
+            Logger.logErr("Error while unloading natives: " + ex, ex);
+            return false;
+        }
+    }
+    
+    public static final boolean isNativesLoaded() {
+        return nativesLoaded;
+    }
+    
     private static final void registerNatives() {
-        System.setProperty(LWJGLLIBRARYPATH, nativesFolder.getAbsolutePath());
+        System.setProperty(LWJGLLIBRARYPATH, NATIVESFOLDER.getAbsolutePath());
         Logger.log("Registered natives successfully", LogLevel.FINER);
     }
     
