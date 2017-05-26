bf82fa2c226972feca1041b34e79d874dbe40a4e
Paul Hagedorn <panzer1119@gmail.com>
2017-05-26 18:07:22 +0200

    Finished source of demo

--- src/omnikryptec/test/JBulletTest.java
+++ src/omnikryptec/test/JBulletTest.java
@@ -19,13 +19,15 @@ import com.bulletphysics.linearmath.DefaultMotionState;
 import com.bulletphysics.linearmath.MotionState;
 import com.bulletphysics.linearmath.Transform;
 import java.util.HashMap;
+import javax.vecmath.Matrix3f;
 import javax.vecmath.Matrix4f;
 import javax.vecmath.Quat4f;
 import javax.vecmath.Vector3f;
 import omnikryptec.camera.Camera;
-import omnikryptec.camera.MatrixMath;
 import omnikryptec.display.DisplayManager;
 import omnikryptec.display.GameSettings;
+import omnikryptec.event.EventSystem;
+import omnikryptec.event.EventType;
 import omnikryptec.logger.Logger;
 import omnikryptec.main.OmniKryptecEngine;
 import omnikryptec.main.Scene;
@@ -37,6 +39,7 @@ import omnikryptec.texture.Texture;
 import omnikryptec.util.InputUtil;
 import omnikryptec.util.NativesLoader;
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 import org.lwjgl.util.glu.Sphere;
 
 /**
@@ -113,18 +116,78 @@ public class JBulletTest {
         //GL11.glEnd();*/
     }
     
+    private static void logic() {
+        dynamicsWorld.stepSimulation(1 / DisplayManager.instance().getFPS());
+        final HashMap<Entity, RigidBody> ballsToBeRemoved = new HashMap<>();
+        for(Entity e : balls.keySet()) {
+            final RigidBody body = balls.get(e);
+            final Vector3f position = body.getMotionState().getWorldTransform(new Transform()).origin;
+            if(!body.equals(controlBall) && (position.x < -50 || position.x > 50 || position.z < -50 || position.z > 50)) {
+                ballsToBeRemoved.put(e, body);
+            }
+        }
+        for(Entity e : ballsToBeRemoved.keySet()) {
+            final RigidBody body = ballsToBeRemoved.get(e);
+            OmniKryptecEngine.instance().getCurrentScene().removeGameObject(e);
+            balls.remove(e);
+            dynamicsWorld.removeRigidBody(body);
+        }
+        if(applyForce) {
+            final Transform controlBallTransform = new Transform();
+            controlBall.getMotionState().getWorldTransform(controlBallTransform);
+            final Vector3f controlBallLocation = controlBallTransform.origin;
+            final Vector3f cameraPosition = convertVector3fFromLWJGL(OmniKryptecEngine.getInstance().getCurrentScene().getCamera().getRelativePos());
+            final Vector3f force = new Vector3f();
+            force.sub(cameraPosition, controlBallLocation);
+            controlBall.activate(true);
+            controlBall.applyCentralForce(force);
+        }
+        if(createNewShape) {
+            final CollisionShape shape = new SphereShape(3.0F); //m
+            final DefaultMotionState motionState = new DefaultMotionState(new Transform(new Matrix4f(new Quat4f(0, 0, 0, 1), new Vector3f(OmniKryptecEngine.instance().getCurrentScene().getCamera().getRelativePos().x, 35F, OmniKryptecEngine.instance().getCurrentScene().getCamera().getRelativePos().z), 1)));
+            final Vector3f inertia = new Vector3f();
+            shape.calculateLocalInertia(1.0F, inertia);
+            final RigidBodyConstructionInfo constructionInfo = new RigidBodyConstructionInfo(1.0F, motionState, shape, inertia);
+            constructionInfo.restitution = 0.75F;
+            final RigidBody body = new RigidBody(constructionInfo);
+            dynamicsWorld.addRigidBody(body);
+            balls.put(createBallEntity(body), body);
+            createNewShape = false;
+        }
+        if(resetControlBall) {
+            controlBall.setCenterOfMassTransform(DEFAULT_BALL_TRANSFORM);
+            controlBall.setAngularVelocity(new Vector3f(0, 0, 0));
+            controlBall.setLinearVelocity(new Vector3f(0, 0, 0));
+            resetControlBall = false;
+        }
+    }
+    
+    private static void input() {
+        applyForce = InputUtil.isKeyDown(Keyboard.KEY_F);
+        createNewShape = InputUtil.isKeyDown(Keyboard.KEY_N);
+        resetControlBall = InputUtil.isKeyDown(Keyboard.KEY_R);
+    }
+    
     private static Entity createBallEntity(RigidBody body) {
         return new Entity(brunnen_tm) {
             
             @Override
             public void doLogic() {
-                final Vector3f ballPosition = body.getWorldTransform(new Transform()).origin;
+                final Vector3f ballPosition = body.getMotionState().getWorldTransform(new Transform()).origin;
                 setRelativePos(ballPosition.x, ballPosition.y, ballPosition.z);
                 final Quat4f ballOrientation = body.getOrientation(new Quat4f());
                 setRotation(new org.lwjgl.util.vector.Vector3f(ballOrientation.x, ballOrientation.y, ballOrientation.z));
             }
             
         };
+    }
+    
+    private static org.lwjgl.util.vector.Vector3f convertVector3fToLWJGL(Vector3f vector) {
+        return new org.lwjgl.util.vector.Vector3f(vector.x, vector.y, vector.z);
+    }
+    
+    private static Vector3f convertVector3fFromLWJGL(org.lwjgl.util.vector.Vector3f vector) {
+        return new Vector3f(vector.x, vector.y, vector.z);
     }
     
     private static Model brunnen_model;
@@ -157,7 +220,7 @@ public class JBulletTest {
             OmniKryptecEngine.instance().getCurrentScene().getCamera().getRelativePos().y += 3;
             OmniKryptecEngine.instance().getCurrentScene().getCamera().getRelativeRotation().x = 40;
             entity_1.setRelativePos(0, 0, -5);
-            //EventSystem.instance().addEventHandler(e -> render(), EventType.RENDER_EVENT);
+            EventSystem.instance().addEventHandler(e -> {input(); logic();}, EventType.RENDER_EVENT);
             /*OmniKryptecEngine.instance().getCurrentScene().addGameObject(new Entity(brunnen_tm) {
                 
                 @Override
