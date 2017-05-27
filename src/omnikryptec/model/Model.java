fb5bb60b7c292094144965a68c468740df117ffa
pcfreak9000 <roman.borris@gmail.com>
2017-05-27 02:15:24 +0200

    changed model behaviour

--- src/omnikryptec/model/Model.java
+++ src/omnikryptec/model/Model.java
@@ -10,11 +10,6 @@ import omnikryptec.objConverter.ObjLoader;
 public class Model {
 
     private VertexArrayObject vao;	
-//	public Model(ModelData data){
-//		vao = VertexArrayObject.create();
-//		vao.storeData(data.getIndices(), data.getIndices().length, data.getVertices(), data.getTextureCoords(), data.getNormals());
-//		nm = false;
-//	}
 
     public Model(ModelData data){
         vao = VertexArrayObject.create();
@@ -29,17 +24,6 @@ public class Model {
         return vao;
     }
 
-    private static final int QUAD_VERTEX_COUNT = 4;
-    //private static final float[] QUAD_VERTICES = {0, 0, 1, 0, 1, 1, 0, 1};
-    private static final float[] QUAD_VERTICES = { -1, 1, -1, -1, 1, 1, 1, -1 };
-    private static final float[] QUAD_TEX_COORDS = {0,0,0,1,1,1,1,0};
-    private static final int[] QUAD_INDICES = {0,3,1,1,3,2};
-
-    public static Model generateQuad() {
-        VertexArrayObject vao = VertexArrayObject.create();
-        vao.storeData(QUAD_INDICES, QUAD_VERTEX_COUNT, QUAD_VERTICES, QUAD_TEX_COORDS);
-        return new Model(vao);
-    }
     
     public static Model newModel(File file) {
         try {
