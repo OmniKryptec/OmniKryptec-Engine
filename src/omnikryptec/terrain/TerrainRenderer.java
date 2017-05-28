f9e6a023c92f4ed4e7b476481345b26667f99ef2
Paul Hagedorn <panzer1119@gmail.com>
2017-05-28 18:48:26 +0200

    Auto stash before merge of "test" and "origin/test"

--- src/omnikryptec/terrain/TerrainRenderer.java
+++ src/omnikryptec/terrain/TerrainRenderer.java
@@ -2,6 +2,7 @@ package omnikryptec.terrain;
 
 import java.util.List;
 import omnikryptec.entity.Entity;
+import omnikryptec.logger.LogEntry.LogLevel;
 import omnikryptec.logger.Logger;
 import omnikryptec.main.Scene;
 import omnikryptec.model.TexturedModel;
@@ -32,45 +33,30 @@ public class TerrainRenderer implements IRenderer {
     private TexturedModel model;
 
     @Override
-    public void render(Scene s, RenderMap entities) {
-        Logger.log("Started Terrain Rendering");
+    public void render(Scene s, RenderMap<TexturedModel, List<Entity>> entities) {
         shader.start();
         TerrainShader.viewMatrix.loadMatrix(s.getCamera().getViewMatrix());
         TerrainShader.projectionMatrix.loadMatrix(s.getCamera().getProjectionMatrix());
-        TerrainShader.lightPosition.loadVec3(s.getCamera().getAbsolutePos());
         for(int i = 0; i < entities.keysArray().length; i++) {
             model = entities.keysArray()[i];
             model.getModel().getVao().bind(0, 1, 2);
             model.getTexture().bindToUnit(0);
-            //model.getMaterial().getNormalmap().bindToUnit(1);
             if(model.getMaterial().hasTransparency()) {
                 RenderUtil.cullBackFaces(false);
             }
-            /*
-            if(model.getMaterial().getSpecularmap() != null) {
-                model.getMaterial().getSpecularmap().bindToUnit(2);
-                TerrainShader.hasspecular.loadBoolean(true);
-            } else {
-                TerrainShader.hasspecular.loadBoolean(false);
-            }
-            */
-            TerrainShader.lightColour.loadVec3(1, 1, 1);
-            TerrainShader.shineDamper.loadFloat(0);
-            TerrainShader.reflectivity.loadFloat(model.getMaterial().getReflectivity());
             stapel = entities.get(model);
             for(int j = 0; j < stapel.size(); j++) {
                 Entity entity = stapel.get(j);
                 if(entity instanceof Terrain) {
                     terrain = (Terrain) entity;
-                    Logger.log("Found Terrain");
                 } else {
-                    Logger.log("Found Non-Terrain");
+                    Logger.log("Non-Terrain GameObject tried to be rendered as a Terrain, but it failed", LogLevel.WARNING);
                     continue;
                 }
-                //if(RenderUtil.inRenderRange(terrain, s.getCamera())) {
+                if(RenderUtil.inRenderRange(terrain, s.getCamera())) {
                     TerrainShader.transformationMatrix.loadMatrix(Maths.createTransformationMatrix(new Vector3f(terrain.getX(), 0, terrain.getZ()), Constants.MATHS_ZERO, Constants.MATHS_ONE));
                     GL11.glDrawElements(GL11.GL_TRIANGLES, model.getModel().getVao().getIndexCount(), GL11.GL_UNSIGNED_INT, 0);
-                //}
+                }
             }
             stapel = null;
             model.getModel().getVao().unbind(0, 1, 2);
@@ -79,8 +65,6 @@ public class TerrainRenderer implements IRenderer {
             }
         }
     }
-
-
 
     @Override
     public void cleanup() {
--- src/omnikryptec/terrain/TerrainShader.java
+++ src/omnikryptec/terrain/TerrainShader.java
@@ -1,11 +1,8 @@
 package omnikryptec.terrain;
 
 import omnikryptec.shader.Shader;
-import omnikryptec.shader.UniformBoolean;
-import omnikryptec.shader.UniformFloat;
 import omnikryptec.shader.UniformMatrix;
 import omnikryptec.shader.UniformSampler;
-import omnikryptec.shader.UniformVec3;
 
 /**
  *
@@ -13,30 +10,20 @@ import omnikryptec.shader.UniformVec3;
  */
 public class TerrainShader extends Shader {
     
-    private static final String SHADER_FOLDER = "/omnikryptec/terrain/"; //TODO Move?
+    private static final String SHADER_FOLDER = "/omnikryptec/terrain/";
 
-    /*VertexShader*/
+    /*VertexShader Uniforms*/
     public static final UniformMatrix transformationMatrix = new UniformMatrix("transformationMatrix");
     public static final UniformMatrix projectionMatrix = new UniformMatrix("projectionMatrix");
     public static final UniformMatrix viewMatrix = new UniformMatrix("viewMatrix");
-    public static final UniformVec3 lightPosition = new UniformVec3("lightPosition");
     
-    /*FragmentShader*/
+    /*FragmentShader Uniforms*/
     public static final UniformSampler modelTexture = new UniformSampler("modelTexture");
-    public static final UniformVec3 lightColour = new UniformVec3("lightColour");
-    public static final UniformFloat shineDamper = new UniformFloat("shineDamper");
-    public static final UniformFloat reflectivity = new UniformFloat("reflectivity");
-    
-    //public static final UniformBoolean hasspecular = new UniformBoolean("hasspecular");
-    //public static final UniformSampler normalmap = new UniformSampler("normaltex");
-    //public static final UniformSampler specularmap = new UniformSampler("speculartex");
     
     public TerrainShader() {
-        super(TerrainShader.class.getResourceAsStream(SHADER_FOLDER + "terrainVertexShader.txt"), TerrainShader.class.getResourceAsStream(SHADER_FOLDER + "terrainFragmentShader.txt"), "position", "textureCoordinates", "normal", "tangents", transformationMatrix, projectionMatrix, viewMatrix, lightPosition, modelTexture, lightColour, shineDamper, reflectivity);
+        super(TerrainShader.class.getResourceAsStream(SHADER_FOLDER + "terrainVertexShader.txt"), TerrainShader.class.getResourceAsStream(SHADER_FOLDER + "terrainFragmentShader.txt"), "position", "textureCoordinates", "normal", "tangents", transformationMatrix, projectionMatrix, viewMatrix, modelTexture);
         start();
         modelTexture.loadTexUnit(0);
-        //normalmap.loadTexUnit(1);
-        //specularmap.loadTexUnit(2);
     }
     
 }
