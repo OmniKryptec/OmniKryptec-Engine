1d6f3f39cad0ffdade9e9928fbb04a90d16a205a
Paul Hagedorn <panzer1119@gmail.com>
2017-06-21 19:05:37 +0200

    Buggy

--- src/omnikryptec/animation/renderer/AnimatedModelRenderer.java
+++ src/omnikryptec/animation/renderer/AnimatedModelRenderer.java
@@ -36,47 +36,9 @@ public class AnimatedModelRenderer implements Renderer {
 		this.shader = new AnimatedModelShader();
 	}
 
-	/**
-	 * Renders an animated entity. The main thing to note here is that all the
-	 * joint transforms are loaded up to the shader to a uniform array. Also 5
-	 * attributes of the VAO are enabled before rendering, to include joint
-	 * indices and weights.
-	 * 
-	 * @param entity
-	 *            - the animated entity to be rendered.
-	 * @param camera
-	 *            - the camera used to render the entity.
-	 * @param lightDir
-	 *            - the direction of the light in the scene.
-	 */
-	private void render(Entity entity) {
-                RenderUtil.antialias(true);
-                RenderUtil.disableBlending();
-                RenderUtil.enableDepthTesting(true);
-                AnimatedModel animatedModel = (AnimatedModel) entity.getAdvancedModel();
-                
-                Logger.log("Started Rendering: " + entity);
-                
-		animatedModel.getModel().getVao().bind(0, 1, 2, 3, 4);
-                Logger.log("Rendering Test 1: " + entity);
-		animatedModel.getTexture().bindToUnit(0);
-                Logger.log("Rendering Test 2: " + entity);
-		shader.jointTransforms.loadMatrixArray(animatedModel.getJointTransforms());
-                Logger.log("Rendering Test 3: " + entity);
-                try {
-                    Thread.sleep(1000);
-                } catch (Exception ex) {
-                    
-                }
-		GL11.glDrawElements(GL11.GL_TRIANGLES, animatedModel.getModel().getVao().getIndexCount(), GL11.GL_UNSIGNED_INT, 0);
-                Logger.log("Rendering Test 4: " + entity);
-		animatedModel.getModel().getVao().unbind(0, 1, 2, 3, 4);
-                
-                Logger.log("Finished Rendering: " + entity);
-	}
-
 	private List<Entity> stapel;
 	private AdvancedModel model;
+        private AnimatedModel animatedModel;
         
     @Override
     public void render(Scene s, RenderMap<AdvancedModel, List<Entity>> entities) {
@@ -86,17 +48,26 @@ public class AnimatedModelRenderer implements Renderer {
         shader.lightDirection.loadVec3(LIGHT_DIR);
         for(int i = 0; i < entities.keysArray().length; i++) {
             model = entities.keysArray()[i];
-            if(!(model instanceof AdvancedModel)) {
+            if(!(model instanceof AnimatedModel)) {
                 continue;
             }
+            animatedModel = (AnimatedModel) model;
+            model.getModel().getVao().bind(0, 1, 2, 3, 4);
+            RenderUtil.antialias(true);
+            RenderUtil.disableBlending();
+            RenderUtil.enableDepthTesting(true);
             stapel = entities.get(model);
             if(stapel != null && !stapel.isEmpty()) {
                 stapel.stream().forEach((entity) -> {
                     if(entity.isActive() && RenderUtil.inRenderRange(entity, camera)) {
-                        render(entity);
+                        animatedModel.getTexture().bindToUnit(0);
+                        shader.jointTransforms.loadMatrixArray(animatedModel.getJointTransforms());
+                        GL11.glDrawElements(GL11.GL_TRIANGLES, animatedModel.getModel().getVao().getIndexCount(), GL11.GL_UNSIGNED_INT, 0);
                     }
                 });
             }
+            stapel = null;
+            model.getModel().getVao().unbind(0, 1, 2, 3, 4);
         }
     }
 
