ba8ebf98c2e3ac562c085c376cfe4f4e821bda9b
Paul Hagedorn <panzer1119@gmail.com>
2017-06-25 14:48:43 +0200

    Little beauty changes

--- src/omnikryptec/loader/ResourceLoader.java
+++ src/omnikryptec/loader/ResourceLoader.java
@@ -13,54 +13,55 @@ import omnikryptec.util.ArrayUtil;
 
 /**
  * ResourceLoader
+ *
  * @author Panzer1119 &amp; pcfreak9000
  */
 public class ResourceLoader implements Loader {
-    
+
 //    private ExecutorService executor = null;
     private final HashMap<String, ResourceObject> loadedData = new HashMap<>();
     private final HashMap<Integer, ArrayList<AdvancedFile>> priorityStagedAdvancedFiles = new HashMap<>();
     private final ArrayList<Loader> loaders = new ArrayList<>();
     private String[] extensions = null;
     private String[] blacklist = null;
-    
+
     @Override
-    public ResourceObject load(AdvancedFile advancedFile){
-    	return loadp(advancedFile, advancedFile);
+    public ResourceObject load(AdvancedFile advancedFile) {
+        return loadIntern(advancedFile, advancedFile);
     }
-    
-    public ResourceLoader addRessourceObject(String name, ResourceObject obj){
-    	loadedData.put(name, obj);
-    	return this;
+
+    public ResourceLoader addRessourceObject(String name, ResourceObject resourceObject) {
+        loadedData.put(name, resourceObject);
+        return this;
     }
-    
-    final ResourceObject loadp(AdvancedFile advancedFile, AdvancedFile superfile) {
+
+    final ResourceObject loadIntern(AdvancedFile advancedFile, AdvancedFile superFile) {
         try {
-            if(advancedFile == null || !advancedFile.exists()) {
+            if (advancedFile == null || !advancedFile.exists()) {
                 return null;
             }
-            if(advancedFile.isDirectory()) {
+            if (advancedFile.isDirectory()) {
                 advancedFile.listAdvancedFiles().stream().forEach((af) -> {
-                    loadp(af, superfile);
+                    loadIntern(af, superFile);
                 });
                 return null;
             } else {
                 final List<Loader> loadersForExtension = getLoaderForExtensions(advancedFile.getExtension());
-                ResourceObject dataObj = null;
+                ResourceObject resourceObject = null;
                 String name = null;
-                for(Loader loader : loadersForExtension) {
-                	dataObj = loader.load(advancedFile);
-                	name = loader.generateName(advancedFile, superfile);
-                    if(dataObj != null) {
+                for (Loader loader : loadersForExtension) {
+                    resourceObject = loader.load(advancedFile);
+                    name = loader.generateName(advancedFile, superFile);
+                    if (resourceObject != null && name != null && !name.isEmpty()) {
                         break;
                     }
                 }
-                if(dataObj!=null && name!=null) {
-                	addRessourceObject(name, dataObj);
+                if (resourceObject != null && name != null && !name.isEmpty()) {
+                    addRessourceObject(name, resourceObject);
                 } else {
-                    Logger.log("Failed to load: " + advancedFile, LogLevel.WARNING);
+                    Logger.log(String.format("Failed to load: \"%s\"%s", advancedFile, (advancedFile.getPath().equals(superFile.getPath()) ? "" : String.format(" (in \"%s\")", superFile))), LogLevel.WARNING);
                 }
-                return dataObj;
+                return resourceObject;
             }
         } catch (Exception ex) {
             Logger.logErr("Error while loading staged advanced file: " + ex, ex);
@@ -68,17 +69,17 @@ public class ResourceLoader implements Loader {
         }
     }
 
-	public final void loadStagedAdvancedFiles(boolean clearData/*, long timeout, TimeUnit unit*/) {
+    public final void loadStagedAdvancedFiles(boolean clearData/*, long timeout, TimeUnit unit*/) {
         try {
 //         	resetExecutor();
-            if(clearData){
-            	loadedData.clear();
+            if (clearData) {
+                loadedData.clear();
             }
             final ArrayList<AdvancedFile> stagedAdvancedFiles = getStagedAdvancedFilesSorted();
             stagedAdvancedFiles.stream().forEach((advancedFile) -> {
-            	load(advancedFile);
+                load(advancedFile);
 //                executor.submit(() -> {
-//                    loadp(advancedFile, advancedFile);
+//                    loadIntern(advancedFile, advancedFile);
 //                });
             });
 //          executor.shutdown();
@@ -90,7 +91,7 @@ public class ResourceLoader implements Loader {
 
     @Override
     public final String[] getExtensions() {
-        if(extensions == null) {
+        if (extensions == null) {
             extensions = generateExtensions();
         }
         return extensions;
@@ -98,58 +99,58 @@ public class ResourceLoader implements Loader {
 
     @Override
     public final String[] getBlacklist() {
-        if(blacklist == null) {
+        if (blacklist == null) {
             blacklist = generateBlacklist();
         }
         return blacklist;
     }
-    
+
     public final Loader addLoader(Loader loader) {
-        if(loader == null || loader.equals(this) || loader instanceof ResourceLoader) {
+        if (loader == null || loader.equals(this) || loader instanceof ResourceLoader) {
             return this;
         }
         resetValues();
         loaders.add(loader);
         return this;
     }
-    
+
     public final Loader removeLoader(Loader loader) {
-        if(loader == null || loader.equals(this) || loader instanceof ResourceLoader) {
+        if (loader == null || loader.equals(this) || loader instanceof ResourceLoader) {
             return this;
         }
         resetValues();
         loaders.remove(loader);
         return this;
     }
-    
+
     public final Loader clearLoaders() {
         resetValues();
         loaders.clear();
         return this;
     }
-    
+
     public final Loader stageAdvancedFiles(AdvancedFile... advancedFiles) {
         return stageAdvancedFiles(0, advancedFiles);
     }
-    
+
     public final Loader stageAdvancedFiles(int priority, AdvancedFile... advancedFiles) {
-        if(advancedFiles == null || advancedFiles.length == 0) {
+        if (advancedFiles == null || advancedFiles.length == 0) {
             return this;
         }
         ArrayList<AdvancedFile> stagedAdvancedFiles = priorityStagedAdvancedFiles.get(priority);
-        if(stagedAdvancedFiles == null) {
+        if (stagedAdvancedFiles == null) {
             stagedAdvancedFiles = new ArrayList<>();
             priorityStagedAdvancedFiles.put(priority, stagedAdvancedFiles);
         }
         stagedAdvancedFiles.addAll(Arrays.asList(advancedFiles));
         return this;
     }
-    
+
     public final Loader clearStagedAdvancedFiles() {
         priorityStagedAdvancedFiles.clear();
         return this;
     }
-    
+
     public final ArrayList<AdvancedFile> getStagedAdvancedFilesSorted() {
         final ArrayList<AdvancedFile> stagedAdvancedFiles = new ArrayList<>();
         priorityStagedAdvancedFiles.keySet().stream().sorted((i1, i2) -> i2 - i1).forEach((i) -> {
@@ -157,67 +158,66 @@ public class ResourceLoader implements Loader {
         });
         return stagedAdvancedFiles;
     }
-    
+
     public final List<Loader> getLoaderForExtensions(String... extensions) {
         return loaders.stream().filter((loader) -> ArrayUtil.contains(loader.getExtensions(), extensions)).collect(Collectors.toList());
     }
-    
+
     @SuppressWarnings("unchecked")
-	public final <T extends ResourceObject> T getData(String name) {
-    	if(name == null || name.isEmpty()) {
+    public final <T extends ResourceObject> T getData(String name) {
+        if (name == null || name.isEmpty()) {
             return null;
         }
-    	try{
-    		return (T) loadedData.get(name);
-    	}catch(ClassCastException ex){
-    		return null;
-    	}
-    	
+        try {
+            return (T) loadedData.get(name);
+        } catch (ClassCastException ex) {
+            return null;
+        }
+
     }
-    
+
     @SuppressWarnings("unchecked")
-	public final <T extends ResourceObject> T getData(Class<? extends T> c, String name) {
-    	if(c == null || name == null || name.isEmpty()) {
+    public final <T extends ResourceObject> T getData(Class<? extends T> c, String name) {
+        if (c == null || name == null || name.isEmpty()) {
             return null;
         }
         ResourceObject data = loadedData.get(name);
-        if(data == null || !c.isAssignableFrom(data.getClass())) { //TODO Gucken ob das isAssignableFrom so richtig herum ist
+        if (data == null || !c.isAssignableFrom(data.getClass())) { //TODO Gucken ob das isAssignableFrom so richtig herum ist
             return null;
         }
         return (T) data;
     }
-    
+
     public final <T extends ResourceObject> ArrayList<T> getAllData(Class<? extends T> c) {
-    	return getAllData(c, null);
+        return getAllData(c, null);
     }
 
-    
     @SuppressWarnings("unchecked")
-	private final <T extends ResourceObject> ArrayList<T> getAllData(Class<? extends T> c, ArrayList<T> dataa) {
-        if(dataa==null){
-        	dataa = new ArrayList<>();
+    private final <T extends ResourceObject> ArrayList<T> getAllData(Class<? extends T> c, ArrayList<T> dataOld) {
+        if (dataOld == null) {
+            dataOld = new ArrayList<>();
         }
-        final ArrayList<T> data = dataa;
-    	List<ResourceObject> d = loadedData.values().stream().filter((object) -> (object != null && c.isAssignableFrom(object.getClass()))).collect(Collectors.toList()); //TODO Gucken ob das isAssignableFrom so richtig herum ist
+        final ArrayList<T> data = dataOld;
+        List<ResourceObject> d = loadedData.values().stream().filter((object) -> (object != null && c.isAssignableFrom(object.getClass()))).collect(Collectors.toList()); //TODO Gucken ob das isAssignableFrom so richtig herum ist
         d.stream().forEach((object) -> {
             data.add((T) object);
         });
         d.clear();
         return data;
     }
-    
+
     private final void resetValues() {
         extensions = null;
         blacklist = null;
     }
-    
+
     private final String[] generateExtensions() {
         final ArrayList<String> extensions_temp = new ArrayList<>();
         loaders.stream().forEach((loader) -> {
             String[] ext = loader.getExtensions();
-            if(ext != null && ext.length > 0) {
-                for(String e : ext) {
-                    if(!extensions_temp.contains(e)) {
+            if (ext != null && ext.length > 0) {
+                for (String e : ext) {
+                    if (!extensions_temp.contains(e)) {
                         extensions_temp.add(e);
                     }
                 }
@@ -225,14 +225,14 @@ public class ResourceLoader implements Loader {
         });
         return extensions_temp.toArray(new String[extensions_temp.size()]);
     }
-    
+
     private final String[] generateBlacklist() {
         final ArrayList<String> blacklist_temp = new ArrayList<>();
         loaders.stream().forEach((loader) -> {
             String[] bl = loader.getBlacklist();
-            if(bl != null && bl.length > 0) {
-                for(String b : bl) {
-                    if(!blacklist_temp.contains(b)) {
+            if (bl != null && bl.length > 0) {
+                for (String b : bl) {
+                    if (!blacklist_temp.contains(b)) {
                         blacklist_temp.add(b);
                     }
                 }
@@ -240,7 +240,7 @@ public class ResourceLoader implements Loader {
         });
         return blacklist_temp.toArray(new String[blacklist_temp.size()]);
     }
-    
+
 //    private final Loader resetExecutor() {
 //        if(executor != null) {
 //            executor.shutdownNow();
@@ -248,6 +248,4 @@ public class ResourceLoader implements Loader {
 //        executor = Executors.newFixedThreadPool(10);
 //        return this;
 //    }
-    
-    
 }
