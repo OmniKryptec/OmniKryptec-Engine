dabe49899bc92d25b053ac803a9966aa4b1e8fc7
Paul Hagedorn <panzer1119@gmail.com>
2017-06-27 02:51:15 +0200

    Updated EventSystem

--- src/omnikryptec/main/OmniKryptecEngine.java
+++ src/omnikryptec/main/OmniKryptecEngine.java
@@ -34,275 +34,274 @@ import omnikryptec.util.RenderUtil;
  */
 public class OmniKryptecEngine {
 
-	private static OmniKryptecEngine instance;
+    private static OmniKryptecEngine instance;
 
-	public static OmniKryptecEngine instance() {
-		if (instance == null) {
-			if (DisplayManager.instance() == null) {
-				throw new IllegalStateException(
-						"Cant create the Engine because the DisplayManager is not created yet!");
-			}
-			new OmniKryptecEngine(DisplayManager.instance());
-		}
-		return instance;
-	}
+    public static OmniKryptecEngine instance() {
+        if (instance == null) {
+            if (DisplayManager.instance() == null) {
+                throw new IllegalStateException(
+                        "Cant create the Engine because the DisplayManager is not created yet!");
+            }
+            new OmniKryptecEngine(DisplayManager.instance());
+        }
+        return instance;
+    }
 
-	public static OmniKryptecEngine getInstance() {
-		return instance;
-	}
+    public static OmniKryptecEngine getInstance() {
+        return instance;
+    }
 
-	public static void addShutdownHook(Runnable run) {
-		if (run == null) {
-			return;
-		}
-		Runtime.getRuntime().addShutdownHook(new Thread(run));
-	}
+    public static void addShutdownHook(Runnable run) {
+        if (run == null) {
+            return;
+        }
+        Runtime.getRuntime().addShutdownHook(new Thread(run));
+    }
 
-	/**
-	 * @param args
-	 *            the command line arguments
-	 */
-	public static void main(String[] args) {
-		// TODO say that this is a library?
-	}
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO say that this is a library?
+    }
 
-	public static enum State {
-		Starting, Running, Error, Stopping, Stopped;
-	}
+    public static enum State {
+        Starting, Running, Error, Stopping, Stopped;
+    }
 
-	public static enum ShutdownOption {
-		JAVA(2), ENGINE(1), NOTHING(0);
+    public static enum ShutdownOption {
+        JAVA(2), ENGINE(1), NOTHING(0);
 
-		private final int level;
+        private final int level;
 
-		private ShutdownOption(int level) {
-			this.level = level;
-		}
+        private ShutdownOption(int level) {
+            this.level = level;
+        }
 
-		public int getLevel() {
-			return level;
-		}
-	}
+        public int getLevel() {
+            return level;
+        }
+    }
 
-	private State state = State.Stopped;
+    private State state = State.Stopped;
 
-	public State getState() {
-		return state;
-	}
+    public State getState() {
+        return state;
+    }
 
-	private DisplayManager manager;
-	private EventSystem eventsystem;
-	private PostProcessing postpro;
-	private final ArrayList<Scene> scenes = new ArrayList<>();
-	private Scene sceneCurrent;
-	private long vertsCountCurrent=0;
-	
-	private ShutdownOption shutdownOption = ShutdownOption.JAVA;
-	private boolean requestclose = false;
+    private DisplayManager manager;
+    private EventSystem eventsystem;
+    private PostProcessing postpro;
+    private final ArrayList<Scene> scenes = new ArrayList<>();
+    private Scene sceneCurrent;
+    private long vertsCountCurrent = 0;
 
-	public OmniKryptecEngine(DisplayManager manager) {
-		if (manager == null) {
-			throw new NullPointerException("DisplayManager is null");
-		}
-		if (instance != null) {
-			throw new IllegalStateException("OmniKryptec-Engine was already created!");
-		}
-		this.manager = manager;
-		state = State.Starting;
-		instance = this;
-		eventsystem = EventSystem.instance();
-		postpro = PostProcessing.instance();
-		RenderUtil.cullBackFaces(true);
-		RenderUtil.enableDepthTesting(true);
-		RendererRegistration.init();
-		createFbos();
-		eventsystem.fireEvent(new Event(), EventType.BOOTING_COMPLETED);
-	}
+    private ShutdownOption shutdownOption = ShutdownOption.JAVA;
+    private boolean requestclose = false;
 
-	private FrameBufferObject scenefbo;
-	private FrameBufferObject unsampledfbo, normalfbo, specularfbo, extrainfofbo;
-	private FrameBufferObject[] add;
+    public OmniKryptecEngine(DisplayManager manager) {
+        if (manager == null) {
+            throw new NullPointerException("DisplayManager is null");
+        }
+        if (instance != null) {
+            throw new IllegalStateException("OmniKryptec-Engine was already created!");
+        }
+        this.manager = manager;
+        state = State.Starting;
+        instance = this;
+        eventsystem = EventSystem.instance();
+        postpro = PostProcessing.instance();
+        RenderUtil.cullBackFaces(true);
+        RenderUtil.enableDepthTesting(true);
+        RendererRegistration.init();
+        createFbos();
+        eventsystem.fireEvent(new Event(), EventType.BOOTING_COMPLETED);
+    }
 
-	private void createFbos() {
-		scenefbo = new FrameBufferObject(Display.getWidth(), Display.getHeight(),
-				manager.getSettings().getMultiSamples(), manager.getSettings().getAddAttachments(),
-				new RenderTarget(GL30.GL_COLOR_ATTACHMENT0), new RenderTarget(GL30.GL_COLOR_ATTACHMENT1),
-				new RenderTarget(GL30.GL_COLOR_ATTACHMENT2, true), new RenderTarget(GL30.GL_COLOR_ATTACHMENT3));
-		unsampledfbo = new FrameBufferObject(Display.getWidth(), Display.getHeight(), DepthbufferType.DEPTH_TEXTURE,
-				new RenderTarget(GL30.GL_COLOR_ATTACHMENT0));
-		normalfbo = new FrameBufferObject(Display.getWidth(), Display.getHeight(), DepthbufferType.NONE,
-				new RenderTarget(GL30.GL_COLOR_ATTACHMENT0));
-		specularfbo = new FrameBufferObject(Display.getWidth(), Display.getHeight(), DepthbufferType.NONE,
-				new RenderTarget(GL30.GL_COLOR_ATTACHMENT0, true));
-		extrainfofbo = new FrameBufferObject(Display.getWidth(), Display.getHeight(), DepthbufferType.NONE,
-				new RenderTarget(GL30.GL_COLOR_ATTACHMENT0));
-		add = manager.getSettings().getAddFBOs();
-	}
+    private FrameBufferObject scenefbo;
+    private FrameBufferObject unsampledfbo, normalfbo, specularfbo, extrainfofbo;
+    private FrameBufferObject[] add;
 
-	private void resizeFbos() {
-		scenefbo.delete();
-		unsampledfbo.delete();
-		normalfbo.delete();
-		specularfbo.delete();
-		extrainfofbo.delete();
-		createFbos();
-	}
+    private void createFbos() {
+        scenefbo = new FrameBufferObject(Display.getWidth(), Display.getHeight(),
+                manager.getSettings().getMultiSamples(), manager.getSettings().getAddAttachments(),
+                new RenderTarget(GL30.GL_COLOR_ATTACHMENT0), new RenderTarget(GL30.GL_COLOR_ATTACHMENT1),
+                new RenderTarget(GL30.GL_COLOR_ATTACHMENT2, true), new RenderTarget(GL30.GL_COLOR_ATTACHMENT3));
+        unsampledfbo = new FrameBufferObject(Display.getWidth(), Display.getHeight(), DepthbufferType.DEPTH_TEXTURE,
+                new RenderTarget(GL30.GL_COLOR_ATTACHMENT0));
+        normalfbo = new FrameBufferObject(Display.getWidth(), Display.getHeight(), DepthbufferType.NONE,
+                new RenderTarget(GL30.GL_COLOR_ATTACHMENT0));
+        specularfbo = new FrameBufferObject(Display.getWidth(), Display.getHeight(), DepthbufferType.NONE,
+                new RenderTarget(GL30.GL_COLOR_ATTACHMENT0, true));
+        extrainfofbo = new FrameBufferObject(Display.getWidth(), Display.getHeight(), DepthbufferType.NONE,
+                new RenderTarget(GL30.GL_COLOR_ATTACHMENT0));
+        add = manager.getSettings().getAddFBOs();
+    }
 
-	public final DisplayManager getDisplayManager() {
-		return manager;
-	}
+    private void resizeFbos() {
+        scenefbo.delete();
+        unsampledfbo.delete();
+        normalfbo.delete();
+        specularfbo.delete();
+        extrainfofbo.delete();
+        createFbos();
+    }
 
-	public final EventSystem getEventsystem() {
-		return eventsystem;
-	}
+    public final DisplayManager getDisplayManager() {
+        return manager;
+    }
 
-	public final PostProcessing getPostprocessor() {
-		return postpro;
-	}
+    public final EventSystem getEventsystem() {
+        return eventsystem;
+    }
 
-	public final void startLoop(ShutdownOption shutdownOption) {
-		setShutdownOption(shutdownOption);
-		state = State.Running;
-		while (!Display.isCloseRequested() && !requestclose && state != State.Error) {
-			frame(true);
-		}
-		close(this.shutdownOption);
-	}
+    public final PostProcessing getPostprocessor() {
+        return postpro;
+    }
 
-	public final OmniKryptecEngine requestClose() {
-		return requestClose(shutdownOption);
-	}
+    public final void startLoop(ShutdownOption shutdownOption) {
+        setShutdownOption(shutdownOption);
+        state = State.Running;
+        while (!Display.isCloseRequested() && !requestclose && state != State.Error) {
+            frame(true);
+        }
+        close(this.shutdownOption);
+    }
 
-	public final OmniKryptecEngine requestClose(ShutdownOption shutdownOption) {
-		setShutdownOption(shutdownOption);
-		requestclose = true;
-		return this;
-	}
+    public final OmniKryptecEngine requestClose() {
+        return requestClose(shutdownOption);
+    }
 
-	public final OmniKryptecEngine frame(boolean clear) {
-		final long currentTime = manager.getCurrentTime();
-		try {
-			if (!Display.isActive()) {
-				Display.update();
-				try {
-					Thread.sleep(1);
-				} catch (InterruptedException e) {
-					errorOccured(e, "Error occured while sleeping: ");
-				}
-				return this;
-			}
-			InputUtil.nextFrame();
-			AudioManager.update(currentTime);
-			if (Display.wasResized()) {
-				resizeFbos();
-				PostProcessing.instance().resize();
-				eventsystem.fireEvent(new Event(manager), EventType.RESIZED);
-			}
-			scenefbo.bindFrameBuffer();
-			if (sceneCurrent != null) {
-				if (clear) {
-					RenderUtil.clear(sceneCurrent.getClearColor());
-				}
-				vertsCountCurrent = sceneCurrent.frame(Float.POSITIVE_INFINITY, Float.NEGATIVE_INFINITY, false, AllowedRenderer.All);
-				ParticleMaster.instance().update(getCurrentScene().getCamera());
-			}
-			eventsystem.fireEvent(new Event(), EventType.RENDER_EVENT);
-			scenefbo.unbindFrameBuffer();
-			scenefbo.resolveToFbo(unsampledfbo, GL30.GL_COLOR_ATTACHMENT0);
-			scenefbo.resolveToFbo(normalfbo, GL30.GL_COLOR_ATTACHMENT1);
-			scenefbo.resolveToFbo(specularfbo, GL30.GL_COLOR_ATTACHMENT2);
-			scenefbo.resolveToFbo(extrainfofbo, GL30.GL_COLOR_ATTACHMENT3);
-			if (sceneCurrent != null) {
-				if (scenefbo.getTargets().length > 4) {
-					for (int i = 4; i < scenefbo.getTargets().length; i++) {
-						scenefbo.resolveToFbo(add[i], manager.getSettings().getAddAttachments()[i - 4].target);
-					}
-				}
-				PostProcessing.instance().doPostProcessing(add, unsampledfbo, normalfbo, specularfbo, extrainfofbo);
-			}
-			eventsystem.fireEvent(new Event(), EventType.FRAME_EVENT);
-			DisplayManager.instance().updateDisplay();
-		} catch (Exception e) {
-			errorOccured(e, "Error occured in frame: ");
-		}
-		return this;
-	}
+    public final OmniKryptecEngine requestClose(ShutdownOption shutdownOption) {
+        setShutdownOption(shutdownOption);
+        requestclose = true;
+        return this;
+    }
 
-	public void errorOccured(Exception e, String text) {
-		state = State.Error;
-		Logger.logErr(text + e, e);
-		eventsystem.fireEvent(new Event(e), EventType.ERROR);
-	}
+    public final OmniKryptecEngine frame(boolean clear) {
+        final long currentTime = manager.getCurrentTime();
+        try {
+            if (!Display.isActive()) {
+                Display.update();
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    errorOccured(e, "Error occured while sleeping: ");
+                }
+                return this;
+            }
+            InputUtil.nextFrame();
+            AudioManager.update(currentTime);
+            if (Display.wasResized()) {
+                resizeFbos();
+                PostProcessing.instance().resize();
+                eventsystem.fireEvent(new Event(manager), EventType.RESIZED);
+            }
+            scenefbo.bindFrameBuffer();
+            if (sceneCurrent != null) {
+                if (clear) {
+                    RenderUtil.clear(sceneCurrent.getClearColor());
+                }
+                vertsCountCurrent = sceneCurrent.frame(Float.POSITIVE_INFINITY, Float.NEGATIVE_INFINITY, false, AllowedRenderer.All);
+                ParticleMaster.instance().update(getCurrentScene().getCamera());
+            }
+            eventsystem.fireEvent(new Event(), EventType.RENDER_EVENT);
+            scenefbo.unbindFrameBuffer();
+            scenefbo.resolveToFbo(unsampledfbo, GL30.GL_COLOR_ATTACHMENT0);
+            scenefbo.resolveToFbo(normalfbo, GL30.GL_COLOR_ATTACHMENT1);
+            scenefbo.resolveToFbo(specularfbo, GL30.GL_COLOR_ATTACHMENT2);
+            scenefbo.resolveToFbo(extrainfofbo, GL30.GL_COLOR_ATTACHMENT3);
+            if (sceneCurrent != null) {
+                if (scenefbo.getTargets().length > 4) {
+                    for (int i = 4; i < scenefbo.getTargets().length; i++) {
+                        scenefbo.resolveToFbo(add[i], manager.getSettings().getAddAttachments()[i - 4].target);
+                    }
+                }
+                PostProcessing.instance().doPostProcessing(add, unsampledfbo, normalfbo, specularfbo, extrainfofbo);
+            }
+            eventsystem.fireEvent(new Event(), EventType.FRAME_EVENT);
+            DisplayManager.instance().updateDisplay();
+        } catch (Exception e) {
+            errorOccured(e, "Error occured in frame: ");
+        }
+        return this;
+    }
 
-	public final long getModelVertsCount(){
-		return vertsCountCurrent;
-	}
-	
-	public final OmniKryptecEngine close(ShutdownOption shutdownOption) {
-		if (shutdownOption.getLevel() >= ShutdownOption.ENGINE.getLevel()) {
-			state = State.Stopping;
-			cleanup();
-			manager.close();
-			state = State.Stopped;
-			if (shutdownOption.getLevel() >= ShutdownOption.JAVA.getLevel()) {
-				Commands.COMMANDEXIT.run("-java");
-			}
-			return null;
-		} else {
-			return this;
-		}
-	}
+    public void errorOccured(Exception e, String text) {
+        state = State.Error;
+        Logger.logErr(text + e, e);
+        eventsystem.fireEvent(new Event(e), EventType.ERROR);
+    }
 
-	private void cleanup() {
-		RenderChunk.cleanup();
-		PostProcessing.cleanup();
-		VertexArrayObject.cleanup();
-		VertexBufferObject.cleanup();
-		FrameBufferObject.cleanup();
-		RendererRegistration.cleanup();
-		ParticleMaster.cleanup();
-		SimpleTexture.cleanup();
-	}
+    public final long getModelVertsCount() {
+        return vertsCountCurrent;
+    }
 
-	public final OmniKryptecEngine addAndSetScene(Scene scene) {
-		addScene(scene);
-		setScene(scene.getName());
-		return this;
-	}
+    public final OmniKryptecEngine close(ShutdownOption shutdownOption) {
+        if (shutdownOption.getLevel() >= ShutdownOption.ENGINE.getLevel()) {
+            state = State.Stopping;
+            cleanup();
+            manager.close();
+            state = State.Stopped;
+            if (shutdownOption.getLevel() >= ShutdownOption.JAVA.getLevel()) {
+                Commands.COMMANDEXIT.run("-java");
+            }
+            return null;
+        } else {
+            return this;
+        }
+    }
 
-	public final OmniKryptecEngine addScene(Scene scene) {
-		if (scene != null) {
-			scenes.add(scene);
-		}
-		return this;
-	}
+    private void cleanup() {
+        RenderChunk.cleanup();
+        PostProcessing.cleanup();
+        VertexArrayObject.cleanup();
+        VertexBufferObject.cleanup();
+        FrameBufferObject.cleanup();
+        RendererRegistration.cleanup();
+        ParticleMaster.cleanup();
+        SimpleTexture.cleanup();
+    }
 
-	public final OmniKryptecEngine setScene(String name) {
-		List<Scene> scenesEquals = scenes.stream().filter((scene) -> scene.getName().equals(name))
-				.collect(Collectors.toList());
-		sceneCurrent = (scenesEquals.isEmpty() ? null : scenesEquals.get(0));
-		return this;
-	}
+    public final OmniKryptecEngine addAndSetScene(Scene scene) {
+        addScene(scene);
+        setScene(scene.getName());
+        return this;
+    }
 
-	public final Scene getCurrentScene() {
-		return sceneCurrent;
-	}
+    public final OmniKryptecEngine addScene(Scene scene) {
+        if (scene != null) {
+            scenes.add(scene);
+        }
+        return this;
+    }
 
-	public final ShutdownOption getShutdownOption() {
-		return shutdownOption;
-	}
+    public final OmniKryptecEngine setScene(String name) {
+        List<Scene> scenesEquals = scenes.stream().filter((scene) -> scene.getName().equals(name))
+                .collect(Collectors.toList());
+        sceneCurrent = (scenesEquals.isEmpty() ? null : scenesEquals.get(0));
+        return this;
+    }
 
-	public final OmniKryptecEngine setShutdownOption(ShutdownOption shutdownOption) {
-		this.shutdownOption = shutdownOption;
-		return this;
-	}
+    public final Scene getCurrentScene() {
+        return sceneCurrent;
+    }
 
-	public final ArrayList<Scene> getScenes() {
-		return scenes;
-	}
+    public final ShutdownOption getShutdownOption() {
+        return shutdownOption;
+    }
 
-	public String getCurrentSceneName() {
-		return sceneCurrent == null ? null : sceneCurrent.getName();
-	}
+    public final OmniKryptecEngine setShutdownOption(ShutdownOption shutdownOption) {
+        this.shutdownOption = shutdownOption;
+        return this;
+    }
+
+    public final ArrayList<Scene> getScenes() {
+        return scenes;
+    }
+
+    public String getCurrentSceneName() {
+        return sceneCurrent == null ? null : sceneCurrent.getName();
+    }
 
 }
