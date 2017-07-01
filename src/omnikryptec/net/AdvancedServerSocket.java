e31f97f464a8e35f683d1baaa12bc7c1677422e8
Paul Hagedorn <panzer1119@gmail.com>
2017-07-01 02:40:21 +0200

    Introducing Server (WIP)

--- src/omnikryptec/net/AdvancedServerSocket.java
+++ src/omnikryptec/net/AdvancedServerSocket.java
@@ -1,9 +1,44 @@
 package omnikryptec.net;
 
+import java.io.Serializable;
+import java.net.ServerSocket;
+import java.time.Instant;
+
 /**
  * AdvancedServerSocket
+ *
  * @author Panzer1119
  */
-public class AdvancedServerSocket {
-    
+public abstract class AdvancedServerSocket implements Serializable {
+
+    private final AdvancedServerSocket ME = this;
+    private ServerSocket serverSocket = null;
+    private int port = -1;
+
+    public AdvancedServerSocket() {
+        
+    }
+
+    /**
+     * Processes Inputs from sockets
+     *
+     * @param object Input
+     * @param timestamp Timestamp
+     */
+    public abstract void processInput(Object object, AdvancedSocket socket, Instant timestamp);
+
+    /**
+     * Called when a connection from a socket was successfully accepted
+     *
+     * @param timestamp Timestamp
+     */
+    public abstract void onConnected(AdvancedSocket socket, Instant timestamp);
+
+    /**
+     * Called when a connection from a socket was disconnected
+     *
+     * @param timestamp Timestamp
+     */
+    public abstract void onDisconnected(AdvancedSocket socket, Instant timestamp);
+
 }
---
+++ src/omnikryptec/net/Server.java
@@ -0,0 +1,31 @@
+package omnikryptec.net;
+
+import java.time.Instant;
+
+/**
+ * Server
+ *
+ * @author Panzer1119
+ */
+public class Server extends AdvancedServerSocket {
+
+    public Server() {
+
+    }
+
+    @Override
+    public void processInput(Object object, AdvancedSocket socket, Instant timestamp) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void onConnected(AdvancedSocket socket, Instant timestamp) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void onDisconnected(AdvancedSocket socket, Instant timestamp) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
