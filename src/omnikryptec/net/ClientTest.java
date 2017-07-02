308d4e0d6fc27ec3186c95eb74817d2ee6250e4e
Paul Hagedorn <panzer1119@gmail.com>
2017-07-02 20:47:12 +0200

    Added testing classes for the net package

---
+++ src/omnikryptec/net/ClientTest.java
@@ -0,0 +1,13 @@
+package omnikryptec.net;
+
+/**
+ * ClientTest
+ * @author Panzer1119
+ */
+public class ClientTest {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ src/omnikryptec/net/ServerTest.java
@@ -0,0 +1,13 @@
+package omnikryptec.net;
+
+/**
+ * ServerTest
+ * @author Panzer1119
+ */
+public class ServerTest {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
