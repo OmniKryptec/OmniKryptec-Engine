b3d8026dbf4b1468a8d47dcef18c652003120b76
pcfreak9000 <roman.borris@gmail.com>
2017-09-17 20:32:45 +0200

    improved PostProcessing to be used with standalone fbos

--- src/omnikryptec/postprocessing/main/PostProcessing.java
+++ src/omnikryptec/postprocessing/main/PostProcessing.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.lwjgl.opengl.GL30;
+
 import omnikryptec.display.DisplayManager;
 import omnikryptec.resource.model.Model;
 import omnikryptec.util.Instance;
@@ -13,7 +15,7 @@ import omnikryptec.util.profiler.Profilable;
 import omnikryptec.util.profiler.ProfileContainer;
 import omnikryptec.util.profiler.Profiler;
 
-public class PostProcessing implements Profilable{
+public class PostProcessing implements Profilable {
 
 	private static List<PostProcessingStage> stages = new ArrayList<>();
 	private static List<FrameBufferObject> beforelist = new ArrayList<>();
@@ -25,34 +27,46 @@ public class PostProcessing implements Profilable{
 	// private FrameBufferObject tmp;
 
 	private boolean enabled = true;
-	private int stagecountactive=0;
-	
+	private int stagecountactive = 0;
+	private FrameBufferObject target;
+	private boolean ismain = false;
+
 	public static PostProcessing instance() {
 		if (instance == null) {
-			new PostProcessing(DisplayManager.instance());
+			Profiler.addProfilable(instance = new PostProcessing(null), 2);
 		}
+		instance.ismain = true;
 		return instance;
 	}
 
-	private PostProcessing(DisplayManager manager) {
-		if (manager == null) {
+	public PostProcessing(FrameBufferObject target) {
+		if (DisplayManager.instance() == null) {
 			throw new NullPointerException("DisplayManager is null");
 		}
-		Profiler.addProfilable(this, 2);
-		instance = this;
+		this.target = target;
 	}
-	
+
 	private double tmptime = 0;
-	private double rendertime=0;
-	
+	private double rendertime = 0;
+
+	public void doPostProcessing(FrameBufferObject... fbo) {
+		this.doPostProcessing(null, fbo);
+	}
+
 	public void doPostProcessing(FrameBufferObject[] fbos, FrameBufferObject... fbo) {
 		before = fbo[0];
 		stagecountactive = 0;
-		rendertime = 0;
+		if (ismain) {
+			rendertime = 0;
+		}
 		if (enabled) {
-			tmptime = Instance.getDisplayManager().getCurrentTime();
+			if (ismain) {
+				tmptime = Instance.getDisplayManager().getCurrentTime();
+			}
 			beforelist.addAll(Arrays.asList(fbo));
-			beforelist.addAll(Arrays.asList(fbos));
+			if (fbos != null) {
+				beforelist.addAll(Arrays.asList(fbos));
+			}
 			start();
 			for (int i = 0; i < stages.size(); i++) {
 				currentStage = stages.get(i);
@@ -70,25 +84,31 @@ public class PostProcessing implements Profilable{
 				}
 			}
 			end();
-			rendertime = Instance.getDisplayManager().getCurrentTime() - tmptime;
+			if (ismain) {
+				rendertime = Instance.getDisplayManager().getCurrentTime() - tmptime;
+			}
 		}
-		before.resolveToScreen();
+		if (target == null) {
+			before.resolveToScreen();
+		} else {
+			before.resolveToFbo(target, GL30.GL_COLOR_ATTACHMENT0, true);
+		}
 		beforelist.clear();
 	}
 
-	public double getRenderTimeMS(){
+	public double getRenderTimeMS() {
 		return rendertime;
 	}
-	
+
 	public PostProcessing setEnabled(boolean b) {
 		enabled = b;
 		return this;
 	}
-	
-	public FrameBufferObject getDisplayedFBO(){
+
+	public FrameBufferObject getDisplayedFBO() {
 		return before;
 	}
-	
+
 	public boolean isEnabled() {
 		return enabled;
 	}
@@ -117,11 +137,11 @@ public class PostProcessing implements Profilable{
 	private void end() {
 		RenderUtil.enableDepthTesting(true);
 	}
-	
-	public int getActiveStageCount(){
+
+	public int getActiveStageCount() {
 		return stagecountactive;
 	}
-	
+
 	public void resize() {
 		for (PostProcessingStage stage : stages) {
 			stage.resize();
@@ -130,7 +150,7 @@ public class PostProcessing implements Profilable{
 
 	@Override
 	public ProfileContainer[] getProfiles() {
-		return new ProfileContainer[]{new ProfileContainer(Profiler.POSTPROCESSOR, getRenderTimeMS())};
+		return new ProfileContainer[] { new ProfileContainer(Profiler.POSTPROCESSOR, getRenderTimeMS()) };
 	}
 
 }
