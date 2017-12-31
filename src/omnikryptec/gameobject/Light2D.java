d52816f396fb5026130ddd189fcce84723e391d5
Unknown <panzer1119@gmail.com>
2017-12-31 14:55:56 +0100

    Updated some libs

--- src/omnikryptec/gameobject/Light2D.java
+++ src/omnikryptec/gameobject/Light2D.java
@@ -3,31 +3,30 @@ package omnikryptec.gameobject;
 import omnikryptec.graphics.SpriteBatch;
 import omnikryptec.resource.texture.Texture;
 
-public class Light2D extends Sprite{
+public class Light2D extends Sprite {
 
-	
-	public Light2D() {
-		this("", null, null);
-	}
-	
-	public Light2D(String name, Texture t) {
-		this(name, t, null);
-	}
-	
-	public Light2D(Texture t, GameObject2D p) {
-		this("", t, p);
-	}
-	
-	public Light2D(String name, GameObject2D p) {
-		this(name, null, p);
-	}
-	
-	public Light2D(String name, Texture texture, GameObject2D parent) {
-		super(name, texture, parent);
-	}
-	
-	@Override
-	public void paint(SpriteBatch batch) {
-		batch.draw(this, true);
-	}
+    public Light2D() {
+        this("", null, null);
+    }
+
+    public Light2D(String name, Texture t) {
+        this(name, t, null);
+    }
+
+    public Light2D(Texture t, GameObject2D p) {
+        this("", t, p);
+    }
+
+    public Light2D(String name, GameObject2D p) {
+        this(name, null, p);
+    }
+
+    public Light2D(String name, Texture texture, GameObject2D parent) {
+        super(name, texture, parent);
+    }
+
+    @Override
+    public void paint(SpriteBatch batch) {
+        batch.draw(this, true);
+    }
 }
--- src/omnikryptec/renderer/d2/DefaultRenderer2D.java
+++ src/omnikryptec/renderer/d2/DefaultRenderer2D.java
@@ -25,100 +25,100 @@ import omnikryptec.util.Instance;
 
 public class DefaultRenderer2D implements Renderer2D, IEventHandler {
 
-	private SpriteBatch batch, finalBatch;
+    private SpriteBatch batch, finalBatch;
 
-	public DefaultRenderer2D() {
-		this(new SpriteBatch());
-	}
+    public DefaultRenderer2D() {
+        this(new SpriteBatch());
+    }
 
-	public DefaultRenderer2D(SpriteBatch batch) {
-		this.batch = batch;
-		this.finalBatch = new SpriteBatch(new Camera().setDefaultScreenSpaceProjection());
-		lights = new FrameBufferObject(Display.getWidth(), Display.getHeight(), DepthbufferType.NONE);
-		fbo = new FrameBufferObject(Display.getWidth(), Display.getHeight(), DepthbufferType.NONE);
-	}
+    public DefaultRenderer2D(SpriteBatch batch) {
+        this.batch = batch;
+        this.finalBatch = new SpriteBatch(new Camera().setDefaultScreenSpaceProjection());
+        lights = new FrameBufferObject(Display.getWidth(), Display.getHeight(), DepthbufferType.NONE);
+        fbo = new FrameBufferObject(Display.getWidth(), Display.getHeight(), DepthbufferType.NONE);
+    }
 
-	private ArrayList<Sprite> sprites;
-	private ArrayList<Light2D> lightlist;
-	private String stringTmp;
-	private FrameBufferObject lights, fbo;
-	private boolean light;
-	private long last = -1;
-	private Color clearcolor2d = new Color(0, 0, 0, 0);
-	private FrustrumFilter filter = new FrustrumFilter();
-	
-	@Override
-	public long render(AbstractScene2D sc, RenderChunk2D global, long camChunkX, long camChunkY, int chunkOffsetX,
-			int chunkOffsetY, HashMap<String, RenderChunk2D> scene) {
-		batch.setCamera(sc.getCamera());
-		filter.setCamera(sc.getCamera());
-		sprites = new ArrayList<>();
-		if (GraphicsUtil.needsUpdate(last, 20)) {
-			light = OmniKryptecEngine.instance().getGameSettings().getBoolean(GameSettings.LIGHT_2D);
-			last = Instance.getFramecount();
-		}
-		sprites.addAll(global.__getSprites());
-		if (light) {
-			lightlist = new ArrayList<>();
-			lightlist.addAll(global.__getLights());
-		}
-		for (long x = -chunkOffsetX; x <= chunkOffsetX; x++) {
-			for (long y = -chunkOffsetY; y <= chunkOffsetY; y++) {
-				stringTmp = Scene2D.xyToString(camChunkX + x, camChunkY + y);
-				if (scene.get(stringTmp) != null) {
-					sprites.addAll(scene.get(stringTmp).__getSprites());
-					if (light) {
-						lightlist.addAll(scene.get(stringTmp).__getLights());
-					}
-				}
-			}
-		}
-		sprites.sort(getLayerComparator());
-		fbo.bindFrameBuffer();
-		GraphicsUtil.clear(clearcolor2d);
-		batch.begin();
-		GraphicsUtil.blendMode(BlendMode.ALPHA);
-		for (Sprite s : sprites) {
-			if(filter.intersects(s)){
-				s.paint(batch);
-			}
-		}
-		batch.end();
-		if (light) {
-			lights.bindFrameBuffer();
-			GraphicsUtil.clear(sc.getAmbientColor());
-			GraphicsUtil.blendMode(BlendMode.ADDITIVE);
-			batch.begin();
-			for (Light2D s : lightlist) {
-				s.paint(batch);
-			}
-			batch.end();
-			lights.unbindFrameBuffer();
-			GraphicsUtil.blendMode(BlendMode.MULTIPLICATIVE);
-			finalBatch.begin();
-			finalBatch.draw(lights, -1, -1, 2, 2);
-			finalBatch.end();
-		}
-		fbo.unbindFrameBuffer();
-		GraphicsUtil.blendMode(BlendMode.ALPHA);
-		finalBatch.begin();
-		finalBatch.draw(fbo, -1, -1, 2, 2);
-		finalBatch.end();
-		return 0;
-	}
+    private ArrayList<Sprite> sprites;
+    private ArrayList<Light2D> lightlist;
+    private String stringTmp;
+    private FrameBufferObject lights, fbo;
+    private boolean light;
+    private long last = -1;
+    private Color clearcolor2d = new Color(0, 0, 0, 0);
+    private FrustrumFilter filter = new FrustrumFilter();
 
-	@Override
-	public void onEvent(Event ev) {
-		if (ev.getType() == EventType.RESIZED) {
-			fbo.delete();
-			lights.delete();
-			lights = new FrameBufferObject(Display.getWidth(), Display.getHeight(), DepthbufferType.NONE);
-			fbo = new FrameBufferObject(Display.getWidth(), Display.getHeight(), DepthbufferType.NONE);
-		}
-	}
+    @Override
+    public long render(AbstractScene2D sc, RenderChunk2D global, long camChunkX, long camChunkY, int chunkOffsetX,
+            int chunkOffsetY, HashMap<String, RenderChunk2D> scene) {
+        batch.setCamera(sc.getCamera());
+        filter.setCamera(sc.getCamera());
+        sprites = new ArrayList<>();
+        if (GraphicsUtil.needsUpdate(last, 20)) {
+            light = OmniKryptecEngine.instance().getGameSettings().getBoolean(GameSettings.LIGHT_2D);
+            last = Instance.getFramecount();
+        }
+        sprites.addAll(global.__getSprites());
+        if (light) {
+            lightlist = new ArrayList<>();
+            lightlist.addAll(global.__getLights());
+        }
+        for (long x = -chunkOffsetX; x <= chunkOffsetX; x++) {
+            for (long y = -chunkOffsetY; y <= chunkOffsetY; y++) {
+                stringTmp = Scene2D.xyToString(camChunkX + x, camChunkY + y);
+                if (scene.get(stringTmp) != null) {
+                    sprites.addAll(scene.get(stringTmp).__getSprites());
+                    if (light) {
+                        lightlist.addAll(scene.get(stringTmp).__getLights());
+                    }
+                }
+            }
+        }
+        sprites.sort(getLayerComparator());
+        fbo.bindFrameBuffer();
+        GraphicsUtil.clear(clearcolor2d);
+        batch.begin();
+        GraphicsUtil.blendMode(BlendMode.ALPHA);
+        for (Sprite s : sprites) {
+            if (filter.intersects(s)) {
+                s.paint(batch);
+            }
+        }
+        batch.end();
+        if (light) {
+            lights.bindFrameBuffer();
+            GraphicsUtil.clear(sc.getAmbientColor());
+            GraphicsUtil.blendMode(BlendMode.ADDITIVE);
+            batch.begin();
+            for (Light2D s : lightlist) {
+                s.paint(batch);
+            }
+            batch.end();
+            lights.unbindFrameBuffer();
+            GraphicsUtil.blendMode(BlendMode.MULTIPLICATIVE);
+            finalBatch.begin();
+            finalBatch.draw(lights, -1, -1, 2, 2);
+            finalBatch.end();
+        }
+        fbo.unbindFrameBuffer();
+        GraphicsUtil.blendMode(BlendMode.ALPHA);
+        finalBatch.begin();
+        finalBatch.draw(fbo, -1, -1, 2, 2);
+        finalBatch.end();
+        return 0;
+    }
 
-	public Color clearColor() {
-		return clearcolor2d;
-	}
+    @Override
+    public void onEvent(Event ev) {
+        if (ev.getType() == EventType.RESIZED) {
+            fbo.delete();
+            lights.delete();
+            lights = new FrameBufferObject(Display.getWidth(), Display.getHeight(), DepthbufferType.NONE);
+            fbo = new FrameBufferObject(Display.getWidth(), Display.getHeight(), DepthbufferType.NONE);
+        }
+    }
+
+    public Color clearColor() {
+        return clearcolor2d;
+    }
 
 }
