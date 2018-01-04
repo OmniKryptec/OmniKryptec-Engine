8452f46f56468ca2bfea8637955040584a193a10
Unknown <panzer1119@gmail.com>
2018-01-04 17:49:22 +0100

    3.1.5: Auto stash before merge of "LWJGL-3.1.5" and "origin/LWJGL-3.1.5"

--- src/omnikryptec/resource/loader/DefaultAnimationLoader.java
+++ src/omnikryptec/resource/loader/DefaultAnimationLoader.java
@@ -3,12 +3,15 @@ package omnikryptec.resource.loader;
 import de.codemakers.io.file.AdvancedFile;
 import omnikryptec.animation.Animation;
 import omnikryptec.animation.loaders.AnimationLoader;
+import omnikryptec.resource.loader.annotations.DefaultLoader;
 import omnikryptec.util.logger.Logger;
 
 /**
  * DefaultAnimationLoader
+ *
  * @author Panzer1119
  */
+@DefaultLoader
 public class DefaultAnimationLoader implements Loader {
 
     @Override
@@ -20,13 +23,8 @@ public class DefaultAnimationLoader implements Loader {
     }
 
     @Override
-    public String[] getExtensions() {
-        return new String[] {"dae"};
+    public LoadingType accept(AdvancedFile advancedFile, AdvancedFile superFile, ResourceLoader resourceLoader) {
+        return advancedFile.getExtension().equalsIgnoreCase("dae") ? LoadingType.OPENGL : LoadingType.NOT; //TODO Kann das hier auch Normal geloaded werden?
     }
 
-    @Override
-    public String[] getBlacklist() {
-        return null;
-    }
-    
 }
