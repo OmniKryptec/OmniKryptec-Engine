bc5c8b2bb867a9e71deff25219b6738e7bc4f67a
Paul Hagedorn <panzer1119@gmail.com>
2018-09-17 00:03:54 +0200

    WIP Entity.java ClassToInstanceMap for Components

--- src/main/java/de/omnikryptec/ecs/Entity.java
+++ src/main/java/de/omnikryptec/ecs/Entity.java
@@ -1,19 +1,23 @@
 package de.omnikryptec.ecs;
 
+import com.google.common.collect.ClassToInstanceMap;
+import com.google.common.collect.MutableClassToInstanceMap;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class Entity {
-	
-	public final long ID;
-	private Map<Integer, Component> comps;
-	
-	public Entity() {
-		this.ID = 0;
-		this.comps = new HashMap<>();
-	}
-	
-	public Map<Integer, Component> getComponents(){
-		return comps;
-	}
+    
+    public final long ID;
+    private Map<Integer, Component> comps;
+    private ClassToInstanceMap<Component> components = MutableClassToInstanceMap.create();
+    
+    public Entity() {
+        this.ID = 0;
+        this.comps = new HashMap<>();
+    }
+    
+    public Map<Integer, Component> getComponents() {
+        return comps;
+    }
 }
