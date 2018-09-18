3e3ac18ae48c705aee0f333adf37f426c73c4454
Paul Hagedorn <panzer1119@gmail.com>
2018-09-18 19:15:03 +0200

    Entity.java removed unused import and added comment
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

--- src/main/java/de/omnikryptec/ecs/Entity.java
+++ src/main/java/de/omnikryptec/ecs/Entity.java
@@ -1,20 +1,19 @@
 package de.omnikryptec.ecs;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-
 import com.google.common.collect.ClassToInstanceMap;
 import com.google.common.collect.MutableClassToInstanceMap;
 
+import java.util.Set;
+
 public class Entity {
     
-    public final long ID; //TODO Please make this lowercase // No, but maybe this will become uneccessary
+    public final long ID; //TODO Please make this lowercase // No, but maybe this will become unnecessary //Yes do it :(
     private ClassToInstanceMap<Component> components;
     
     public Entity() {
         this.ID = 0;
-        this.components = MutableClassToInstanceMap.create();    }
+        this.components = MutableClassToInstanceMap.create();
+    }
     
     public ClassToInstanceMap<Component> getComponents() {
         return components;
