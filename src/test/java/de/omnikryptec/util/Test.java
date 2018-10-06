7725dbf676f1e71387bb300207920f7a432c7de6
pcfreak9000 <roman.borris@gmail.com>
2018-10-06 23:49:17 +0200

    better tests

--- src/test/java/de/omnikryptec/util/Test.java
+++ src/test/java/de/omnikryptec/util/Test.java
@@ -1,17 +1,23 @@
 package de.omnikryptec.util;
 
+import java.util.Iterator;
+
 import de.omnikryptec.util.data.DynamicArray;
 
 public class Test {
 
 	public static void main(String[] args) {
 		DynamicArray<Object> da = new DynamicArray<>();
-		da.add(0, new Object());
-		da.add(1, new Object());
-		da.add(2, new Object());
-		da.remove(2);
-		da.remove(1);
+		da.add(0, "1");
+		da.add(1, "2");
+		da.add(2, "3");
 		da.remove(0);
+		Iterator<Object> it = da.iterator();
+		while(it.hasNext()) {
+			System.out.println(it.next());
+		}
+		da.remove(1);
+		da.remove(2);
 	}
 
 }
