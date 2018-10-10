b2ff967c1e8523d54977c03828982d8d4bd95c37
pcfreak9000 <roman.borris@gmail.com>
2018-10-10 23:41:43 +0200

    added GLBuffer

---
+++ src/main/java/de/omnikryptec/opengl/buffer/GLBuffer.java
@@ -0,0 +1,35 @@
+package de.omnikryptec.opengl.buffer;
+
+import org.lwjgl.opengl.GL15;
+
+public abstract class GLBuffer {
+
+	private final int pointer;
+	private final int type;
+	
+	public GLBuffer(int type) {
+		this.type = type;
+		this.pointer = GL15.glGenBuffers();
+	}
+	
+	public void delete() {
+		GL15.glDeleteBuffers(pointer);
+	}
+	
+	public int id() {
+		return pointer;
+	}
+	
+	public int target() {
+		return type;
+	}
+	
+	public void bind() {
+		GL15.glBindBuffer(type, pointer);
+	}
+	
+	public void unbind() {
+		GL15.glBindBuffer(type, 0);
+	}
+	
+}
