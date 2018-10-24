2023d712358835b7f7cc61cdd6c98c70dc71e87e
pcfreak9000 <roman.borris@gmail.com>
2018-10-24 21:15:44 +0200

    added some javadoc

--- src/main/java/de/omnikryptec/ecs/IECSManager.java
+++ src/main/java/de/omnikryptec/ecs/IECSManager.java
@@ -4,36 +4,75 @@ import java.util.BitSet;
 import java.util.Collection;
 import java.util.List;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 import de.omnikryptec.core.Updateable;
 import de.omnikryptec.ecs.component.Component;
 import de.omnikryptec.ecs.impl.ECSManager;
 import de.omnikryptec.ecs.system.ComponentSystem;
 
 //TODO is updateable useful here?
-public interface IECSManager extends Updateable{
+public interface IECSManager extends Updateable {
 
-	//TODO useful or too much?
+	// TODO useful or too much?
 	public static IECSManager createDefault() {
 		return new ECSManager();
 	}
-	
-	void addEntity(Entity entity);
-	void removeEntity(Entity entity);
-		
-	void addSystem(ComponentSystem system);
-	void removeSystem(ComponentSystem system);
-	
-	List<Entity> getEntitesFor(BitSet f);
-	
-	Collection<Entity> getAll();
-	
+
 	/**
-	 * Called by the entity after it's {@link Component}s changed.
+	 * Adds an {@link Entity} to this {@link IECSManager}.
+	 * 
+	 * @param entity the entity to add
+	 */
+	void addEntity(@Nonnull Entity entity);
+
+	/**
+	 * Removes an {@link Entity} from this {@link IECSManager}
+	 * 
+	 * @param entity the entity to remove
+	 */
+	void removeEntity(@Nonnull Entity entity);
+
+	/**
+	 * Adds a {@link ComponentSystem} to this {@link IECSManager}.
+	 * 
+	 * @param system the system to add
+	 */
+	void addSystem(@Nonnull ComponentSystem system);
+
+	/**
+	 * Removes a {@link ComponentSystem} from this {@link IECSManager}.
+	 * 
+	 * @param system the system to remove
+	 */
+	void removeSystem(@Nonnull ComponentSystem system);
+
+	/**
+	 * Gets all the entities matching a certain {@link Family}.
+	 * 
+	 * @param f the family required
+	 * @return a list of entities matching the requirements
+	 */
+	List<Entity> getEntitesFor(@Nonnull BitSet f);
+
+	/**
+	 * All via {@link #addEntity(Entity)} added entities.
+	 * 
+	 * @return all entities added to this IECSManager
+	 */
+	Collection<Entity> getAll();
+
+	/**
+	 * Called by the entity after it's {@link Component}s changed.<br>
+	 * This method will most likely only be used internaly by the {@link Entity}.
+	 * 
 	 * @param entity the entity those Components changed
 	 */
-	void onEntityComponentsChanged(Entity entity);
+	void onEntityComponentsChanged(@Nonnull Entity entity);
 
-	void addEntityListener(BitSet family, EntityListener listener);
-	void removeEntityListener(BitSet family, EntityListener listener);
-	
+	void addEntityListener(@Nullable BitSet family, @Nonnull EntityListener listener);
+
+	void removeEntityListener(@Nullable BitSet family, @Nonnull EntityListener listener);
+
 }
