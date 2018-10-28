42cb7e5abdc6618dd823553fcdf34ddeee08c861
pcfreak9000 <roman.borris@gmail.com>
2018-10-28 17:42:34 +0100

    improved ExecutorsUtil

--- src/main/java/de/omnikryptec/util/ExecutorsUtil.java
+++ src/main/java/de/omnikryptec/util/ExecutorsUtil.java
@@ -1,16 +1,18 @@
 package de.omnikryptec.util;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ExecutorsUtil {
 
 	private static final int AVAILABLE_PROCESSORS = Runtime.getRuntime().availableProcessors();
-	private static final Collection<ExecutorService> allExecutors = new ArrayList<>();
+	private static final Collection<ExecutorService> allExecutors = new ConcurrentLinkedQueue<ExecutorService>();
+	private static final AtomicBoolean lock = new AtomicBoolean(false);
 
 	public static ExecutorService newFixedThreadPool(int nthreads) {
 		ExecutorService service = Executors.newFixedThreadPool(nthreads);
@@ -25,28 +27,51 @@ public class ExecutorsUtil {
 	}
 
 	public static void register(ExecutorService executorService) {
+		if(lock.get()) {
+			throw new IllegalStateException("Currently shutdowning all");
+		}
 		allExecutors.add(executorService);
 	}
 
 	public static void unregister(ExecutorService executorService) {
+		if(lock.get()) {
+			throw new IllegalStateException("Currently shutdowning all");
+		}
 		allExecutors.remove(executorService);
 	}
-	
+
 	public static void shutdownNow(ExecutorService executorService) {
+		if (lock.get()) {
+			throw new IllegalStateException("Already shutdowning all");
+		}
+		shutdownNowIntern(executorService);
+	}
+
+	private static void shutdownNowIntern(ExecutorService executorService) {
 		executorService.shutdownNow();
 		try {
 			executorService.awaitTermination(1, TimeUnit.MILLISECONDS);
 		} catch (InterruptedException e) {
-			throw new RuntimeException("Awaiting termination failed of"+executorService, e);
+			throw new RuntimeException("Awaiting termination failed of" + executorService, e);
 		}
 	}
-	
+
 	public static void shutdownNowAll() {
-		for (ExecutorService executorService : allExecutors) {
-			shutdownNow(executorService);
+		if (lock.get()) {
+			throw new IllegalStateException("Already shutdowning");
+		}
+		lock.set(true);
+		try {
+			for (ExecutorService executorService : allExecutors) {
+				shutdownNowIntern(executorService);
+			}
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
+		} finally {
+			lock.set(false);
 		}
 	}
-	
+
 	public static int getAvailableProcessors() {
 		return AVAILABLE_PROCESSORS;
 	}
