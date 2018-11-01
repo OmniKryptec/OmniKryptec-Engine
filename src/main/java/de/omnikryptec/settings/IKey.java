9f2953fdd3dc88565fcfd500f06ba8df7dff1649
Paul Hagedorn <panzer1119@gmail.com>
2018-11-01 21:28:53 +0100

    Reimplemented IKey.java and Key.java
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

---
+++ src/main/java/de/omnikryptec/settings/IKey.java
@@ -0,0 +1,45 @@
+/*
+ *    Copyright 2017 - 2018 Roman Borris (pcfreak9000), Paul Hagedorn (Panzer1119)
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package de.omnikryptec.settings;
+
+public interface IKey {
+    
+    /**
+     * Returns the name of the IKey
+     *
+     * @return Name of the IKey
+     */
+    String getName();
+    
+    /**
+     * Returns if this IKey is being pressed
+     *
+     * @return <tt>true</tt> if this IKey is pressed
+     */
+    boolean isPressed();
+    
+    /**
+     * Returns if this IKey is being pressed for a specified time
+     *
+     * @param minTime Float Minimum pressing time
+     * @param maxTime Float Maximum pressing time
+     *
+     * @return <tt>true</tt> if this IKey is pressed for the specified time
+     */
+    boolean isLongPressed(double minTime, double maxTime);
+    
+}
---
+++ src/main/java/de/omnikryptec/settings/Key.java
@@ -0,0 +1,168 @@
+/*
+ *    Copyright 2017 - 2018 Roman Borris (pcfreak9000), Paul Hagedorn (Panzer1119)
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package de.omnikryptec.settings;
+
+import de.codemakers.base.exceptions.NotYetImplementedRuntimeException;
+
+import java.util.Objects;
+
+public class Key implements IKey {
+    
+    private final String name;
+    private int key;
+    private boolean isKeyboardKey;
+    private double lastChange = 0.0F;
+    
+    /**
+     * Constructs a Key (as a keyboard key)
+     *
+     * @param name Name of the Key (e.g. "Arrow Up")
+     * @param key KeyCode (e.g. ((int) 'a'))
+     */
+    public Key(String name, int key) {
+        this(name, key, true);
+    }
+    
+    /**
+     * Constructs a Key
+     *
+     * @param name Name of the Key (e.g. "Arrow Up")
+     * @param key KeyCode (e.g. ((int) 'a'))
+     * @param isKeyboardKey True if the Key is a keyboard key
+     */
+    public Key(String name, int key, boolean isKeyboardKey) {
+        this.name = name;
+        this.key = key;
+        this.isKeyboardKey = isKeyboardKey;
+    }
+    
+    @Override
+    public String getName() {
+        return name;
+    }
+    
+    @Override
+    public boolean isPressed() {
+        if (isKeyboardKey) {
+            //TODO Implement InputManager
+        } else {
+            //TODO Implement InputManager
+        }
+        throw new NotYetImplementedRuntimeException();
+    }
+    
+    @Override
+    public boolean isLongPressed(double minTime, double maxTime) {
+        if (true) {
+            throw new NotYetImplementedRuntimeException();
+        }
+        if (!isPressed()) {
+            return false;
+        }
+        final double currentTime = -1; //TODO Implement time supplier
+        final double pressedTime = currentTime - lastChange;
+        if (pressedTime >= minTime && pressedTime <= maxTime) {
+            this.lastChange = currentTime;
+            return true;
+        }
+        return false;
+    }
+    
+    /**
+     * Returns the KeyCode
+     *
+     * @return KeyCode
+     */
+    public int getKey() {
+        return key;
+    }
+    
+    /**
+     * Sets the KeyCode
+     *
+     * @param key KeyCode (e.g. ((int) 'a'))
+     *
+     * @return A reference to this Key
+     */
+    public Key setKey(int key) {
+        this.key = key;
+        return this;
+    }
+    
+    /**
+     * Returns if this Key is a keyboard key
+     *
+     * @return <tt>true</tt> if this Key is a keyboard key
+     */
+    public boolean isKeyboardKey() {
+        return isKeyboardKey;
+    }
+    
+    /**
+     * @param keyboardKey True if this Key is a keyboard key
+     *
+     * @return A reference to this Key
+     */
+    public Key setKeyboardKey(boolean keyboardKey) {
+        isKeyboardKey = keyboardKey;
+        return this;
+    }
+    
+    /**
+     * Returns the last change of this Key
+     *
+     * @return Last change of this Key
+     */
+    public double getLastChange() { //FIXME Maybe use another System to determine long key presses
+        return lastChange;
+    }
+    
+    /**
+     * Sets the last change of this Key
+     *
+     * @param lastChange Last change of this Key
+     *
+     * @return A reference to this Key
+     */
+    public Key setLastChange(double lastChange) {
+        this.lastChange = lastChange;
+        return this;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || !getClass().isAssignableFrom(o.getClass())) {
+            return false;
+        }
+        final Key key1 = (Key) o;
+        return key == key1.key && isKeyboardKey == key1.isKeyboardKey && Objects.equals(name, key1.name);
+    }
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, key, isKeyboardKey);
+    }
+    
+    @Override
+    public String toString() {
+        return "Key{" + "name='" + name + '\'' + ", key=" + key + ", isKeyboardKey=" + isKeyboardKey + ", lastChange=" + lastChange + '}';
+    }
+    
+}
