2e25f854bf38895ff95ddfc1f6bbdb4fb948ac4f
Paul Hagedorn <panzer1119@gmail.com>
2018-11-01 20:45:54 +0100

    Updated Copyright 2
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

--- src/main/java/de/omnikryptec/graphics/display/Display.java
+++ src/main/java/de/omnikryptec/graphics/display/Display.java
@@ -19,71 +19,71 @@ package de.omnikryptec.graphics.display;
 //TODO unneccessary: just wrapping Window and doing weird stuff here, viewport stuff might be useful though
 @Deprecated
 public class Display {
-
-//	//private Window<?> window;
-//	private int[] viewport = new int[4];
-//	private double aspectratio = -1;
-//
-//	Display(Window<?> window) {
-//		//this.window = window;
-//		calcViewport();
-//		setARViewPort();
-//		// TODO Eventbased? / Input has nothing to do with the Display, move it to a
-//		// different position
-//		InputManager.initCallbacks();
-//		//lastsynced = GLFWManager.active().getTime();
-//	}
-//
-//	void update() {
-//		//window.swapBuffers();
-//		//TODO not implemented yet
-//		//if (window.wasResized()) {
-//			calcViewport();
-//			setARViewPort();
-//		//}
-//	}
-//
-//	
-//
-//	public int getWidth() {
-//		return viewport[2];
-//	}
-//
-//	public int getHeight() {
-//		return viewport[3];
-//	}
-//
-//	// TODO opengl does not belong to this class
-//	// *********************************************************/
-//	public final void resetViewport() {
-//		OpenGL.gl11viewport(0, 0, /*window.getBufferWidth(), window.getBufferHeight()*/0,0);
-//	}
-//
-//	public final void setARViewPort() {
-//		OpenGL.gl11viewport(viewport);
-//	}
-//	// *********************************************************/
-//
-//	public final void resetAspectRatio() {
-//		setAspectRatio(-1);
-//	}
-//
-//	private final void calcViewport() {
-//		viewport = MathUtil.calculateViewport(aspectratio, /*window.getBufferWidth(), window.getBufferHeight()*/0,0);
-//	}
-//
-//	public double getAspectRatio() {
-//		return aspectratio;
-//	}
-//
-//	public int[] getViewportData() {
-//		return viewport;
-//	}
-//
-//	public final void setAspectRatio(double a) {
-//		aspectratio = a;
-//		calcViewport();
-//		setARViewPort();
-//	}
-
+    
+    //	//private Window<?> window;
+    //	private int[] viewport = new int[4];
+    //	private double aspectratio = -1;
+    //
+    //	Display(Window<?> window) {
+    //		//this.window = window;
+    //		calcViewport();
+    //		setARViewPort();
+    //		// TODO Eventbased? / Input has nothing to do with the Display, move it to a
+    //		// different position
+    //		InputManager.initCallbacks();
+    //		//lastsynced = GLFWManager.active().getTime();
+    //	}
+    //
+    //	void update() {
+    //		//window.swapBuffers();
+    //		//TODO not implemented yet
+    //		//if (window.wasResized()) {
+    //			calcViewport();
+    //			setARViewPort();
+    //		//}
+    //	}
+    //
+    //
+    //
+    //	public int getWidth() {
+    //		return viewport[2];
+    //	}
+    //
+    //	public int getHeight() {
+    //		return viewport[3];
+    //	}
+    //
+    //	// TODO opengl does not belong to this class
+    //	// *********************************************************/
+    //	public final void resetViewport() {
+    //		OpenGL.gl11viewport(0, 0, /*window.getBufferWidth(), window.getBufferHeight()*/0,0);
+    //	}
+    //
+    //	public final void setARViewPort() {
+    //		OpenGL.gl11viewport(viewport);
+    //	}
+    //	// *********************************************************/
+    //
+    //	public final void resetAspectRatio() {
+    //		setAspectRatio(-1);
+    //	}
+    //
+    //	private final void calcViewport() {
+    //		viewport = MathUtil.calculateViewport(aspectratio, /*window.getBufferWidth(), window.getBufferHeight()*/0,0);
+    //	}
+    //
+    //	public double getAspectRatio() {
+    //		return aspectratio;
+    //	}
+    //
+    //	public int[] getViewportData() {
+    //		return viewport;
+    //	}
+    //
+    //	public final void setAspectRatio(double a) {
+    //		aspectratio = a;
+    //		calcViewport();
+    //		setARViewPort();
+    //	}
+    
 }
