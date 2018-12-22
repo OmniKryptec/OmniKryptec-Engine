5c35ffb0b9ac46dfd03aca26dd64e38eee8a1ca6
pcfreak9000 <roman.borris@gmail.com>
2018-12-22 22:10:58 +0100

    fix

--- src/main/java/de/omnikryptec/libapi/opengl/GLTexture.java
+++ src/main/java/de/omnikryptec/libapi/opengl/GLTexture.java
@@ -3,5 +3,9 @@ package de.omnikryptec.libapi.opengl;
 import de.omnikryptec.libapi.exposed.render.Texture;
 
 public class GLTexture implements Texture{
+
+    @Override
+    public void bindTexture(int unit) {
+    }
     
 }
