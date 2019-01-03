e30059dc9a0730211b10bb613d82712e6786f3fb
Paul Hagedorn <panzer1119@gmail.com>
2019-01-03 15:09:50 +0100

    Updated copyright header in test
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

--- src/test/java/de/omnikryptec/ecs/SomeOtherSystem.java
+++ src/test/java/de/omnikryptec/ecs/SomeOtherSystem.java
@@ -1,9 +1,25 @@
-package de.omnikryptec.ecs;
+/*
+ *    Copyright 2017 - 2019 Roman Borris (pcfreak9000), Paul Hagedorn (Panzer1119)
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 
-import java.util.BitSet;
+package de.omnikryptec.ecs;
 
 import de.omnikryptec.ecs.system.ComponentSystem;
 import de.omnikryptec.util.updater.Time;
+
+import java.util.BitSet;
 
 public class SomeOtherSystem extends ComponentSystem {
     
--- src/test/java/de/omnikryptec/resource/loader/Test.java
+++ src/test/java/de/omnikryptec/resource/loader/Test.java
@@ -1,11 +1,27 @@
-package de.omnikryptec.resource.loader;
+/*
+ *    Copyright 2017 - 2019 Roman Borris (pcfreak9000), Paul Hagedorn (Panzer1119)
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 
-import java.util.stream.Collectors;
+package de.omnikryptec.resource.loader;
 
 import de.codemakers.io.file.AdvancedFile;
 import de.omnikryptec.resource.loadervpc.LoadingProgressCallback;
 import de.omnikryptec.resource.loadervpc.ResourceLoader;
 import de.omnikryptec.resource.loadervpc.ResourceProcessor;
+
+import java.util.stream.Collectors;
 
 public class Test {
     
