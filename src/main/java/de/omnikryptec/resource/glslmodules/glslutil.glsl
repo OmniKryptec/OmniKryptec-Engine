fb191bfb089a8b84b940782981c451c30c7c6807
pcfreak9000 <roman.borris@gmail.com>
2019-02-09 01:18:02 +0100

    experimental shader random

--- src/main/java/de/omnikryptec/resource/glslmodules/glslutil.glsl
+++ src/main/java/de/omnikryptec/resource/glslmodules/glslutil.glsl
@@ -1,8 +1,15 @@
 $define module random$
 $header$
 float random(vec2 v);
+float random(vec2 v, vec2 d, float n);
+const vec2 c_rand_vec = vec2(12.9898,78.233);
+const float c_rand_float = 43758.5453;
 $header$
 
-float random(vec2 v){
-	return fract(sin(dot(v.xy,vec2(12.9898,78.233)))*43758.5453);
+float random(vec2 v, float n){
+	return random(v, c_rand_vec, n*c_rand_float);
+}
+
+float random(vec2 v, vec2 d, float n){
+	return fract(sin(dot(v.xy,d.xy))*n);
 }
