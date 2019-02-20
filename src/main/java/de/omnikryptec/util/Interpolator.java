9d30207c2f4d4cc5df2232526573ecc4ac36b7c4
Paul Hagedorn <panzer1119@gmail.com>
2019-02-20 21:34:00 +0100

    Improved Interpolator.java enum
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

--- src/main/java/de/omnikryptec/util/Interpolator.java
+++ src/main/java/de/omnikryptec/util/Interpolator.java
@@ -4,39 +4,68 @@ import de.omnikryptec.util.math.Mathd;
 import de.omnikryptec.util.math.Mathf;
 
 public enum Interpolator {
-    None, Linear, Cubic, Quintic, Cos;
+    None {
+        @Override
+        public double interpolate(double t) {
+            return 0.0;
+        }
+        
+        @Override
+        public float interpolate(float t) {
+            return 0.0F;
+        }
+    },
+    Linear {
+        @Override
+        public double interpolate(double t) {
+            return t;
+        }
+        
+        @Override
+        public float interpolate(float t) {
+            return t;
+        }
+    },
+    Cubic {
+        @Override
+        public double interpolate(double t) {
+            return (t * t * (3.0 - 2.0 * t));
+        }
+        
+        @Override
+        public float interpolate(float t) {
+            return (t * t * (3.0F - 2.0F * t));
+        }
+    },
+    Quintic {
+        @Override
+        public double interpolate(double t) {
+            return t * t * t * (t * (t * 6.0 - 15.0) + 10.0);
+        }
+        
+        @Override
+        public float interpolate(float t) {
+            return t * t * t * (t * (t * 6.0F - 15.0F) + 10.0F);
+        }
+    },
+    Cos {
+        @Override
+        public double interpolate(double t) {
+            return (1.0 - Mathd.cos(t * Mathd.PI)) * 0.5;
+        }
+        
+        @Override
+        public float interpolate(float t) {
+            return (1.0F - Mathf.cos(t * Mathf.PI)) * 0.5F;
+        }
+    };
     
     public double interpolate(final double t) {
-        switch (this) {
-        case None:
-            return 0;
-        case Linear:
-            return t;
-        case Cubic:
-            return (t * t * (3 - 2 * t));
-        case Quintic:
-            return t * t * t * (t * (t * 6 - 15) + 10);
-        case Cos:
-            return (1 - Mathd.cos(t * Mathd.PI)) * 0.5;
-        default:
-            throw new IllegalStateException();
-        }
+        throw new AbstractMethodError();
     }
     
     public float interpolate(final float t) {
-        switch (this) {
-        case None:
-            return 0;
-        case Linear:
-            return t;
-        case Cubic:
-            return (t * t * (3 - 2 * t));
-        case Quintic:
-            return t * t * t * (t * (t * 6 - 15) + 10);
-        case Cos:
-            return (1 - Mathf.cos(t * Mathf.PI)) * 0.5f;
-        default:
-            throw new IllegalStateException();
-        }
+        throw new AbstractMethodError();
     }
+    
 }
