2ab8163886fa0e92010ca598a12a2e4eb68f881d
pcfreak9000 <roman.borris@gmail.com>
2019-08-17 17:51:18 +0200

    improved Profiler

--- src/main/java/de/omnikryptec/util/Profiler.java
+++ src/main/java/de/omnikryptec/util/Profiler.java
@@ -7,11 +7,16 @@ import de.omnikryptec.util.data.FixedStack;
 import de.omnikryptec.util.math.Mathd;
 
 public class Profiler {
+    //TODO improve min and max stuff
     
     private static class Struct {
         long sum;
         long count;
+        long max = Long.MIN_VALUE;
+        long min = Long.MAX_VALUE;
         boolean open;
+        
+        long beginTimeTmp;
     }
     
     private static final Map<String, Struct> map = new HashMap<>();
@@ -41,6 +46,7 @@ public class Profiler {
         h.push(id);
         long time = System.nanoTime();
         s.sum += -time;
+        s.beginTimeTmp = time;
     }
     
     //TODO insert specific data (vertex count, flush count, etc)
@@ -51,7 +57,10 @@ public class Profiler {
         if (!s.open) {
             throw new IllegalStateException("never started profiling with ID " + id);
         }
+        long dif = time - s.beginTimeTmp;
         s.sum += time;
+        s.max = Math.max(dif, s.max);
+        s.min = Math.min(dif, s.min);
         s.open = false;
     }
     
@@ -70,6 +79,8 @@ public class Profiler {
             b.append("Count: " + s.count).append('\n');
             b.append("Average time: " + Mathd.round((s.sum / (double) s.count) * 1e-6, 3) + "ms").append('\n');
             b.append("Complete time spend: " + Mathd.round(s.sum * 1e-9, 5) + "s").append('\n');
+            b.append("Min time: " + Mathd.round((s.min) * 1e-6, 3) + "ms").append('\n');
+            b.append("Max time: " + Mathd.round((s.max) * 1e-6, 3) + "ms").append('\n');
         }
         b.append("-------------------");
         return b.toString();
