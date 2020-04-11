f01aa41c2fe2682885b811e65ca47e22d2e46c2a
pcfreak9000 <roman.borris@gmail.com>
2020-04-11 15:40:07 +0200

    added some math utility

--- src/main/java/de/omnikryptec/util/math/Mathf.java
+++ src/main/java/de/omnikryptec/util/math/Mathf.java
@@ -23,46 +23,46 @@ import org.joml.Matrix4f;
 import org.joml.Matrix4fc;
 
 public strictfp class Mathf {
-
+    
     //    public static final Matrix3x2f TMP3x2f = new Matrix3x2f();
     //    public static final Matrix4f TMP4f = new Matrix4f();
-
+    
     public static final Matrix3x2fc IDENTITY3x2f = new Matrix3x2f();
     public static final Matrix4fc IDENTITY4f = new Matrix4f();
-
+    
     public static final float PI = (float) java.lang.Math.PI;
     public static final float E = (float) java.lang.Math.E;
-
+    
     /**
      * All float values above or equal to this value are integer numbers, all float
      * values below or equal to (-1) * this value are integer numbers.
      */
     private static final float TWO_POW_23 = 8388608.0f;
-
+    
     public static float pow(final float in, final float e) {
         return (float) java.lang.Math.pow(in, e);
     }
-
+    
     public static float square(final float x) {
         return x * x;
     }
-
+    
     public static float clamp(final float in, final float min, final float max) {
         return in < min ? min : (in > max ? max : in);
     }
-
+    
     public static float clamp01(final float in) {
         return in < 0.0f ? 0.0f : (in > 1.0f ? 1.0f : in);
     }
-
+    
     public static float interpolate(final float a, final float b, final float ratio, final Interpolator interpol) {
         return lerp(a, b, interpol.interpolate(ratio));
     }
-
+    
     public static float lerp(final float a, final float b, final float ratio) {
         return a * (1 - ratio) + b * ratio;
     }
-
+    
     public static float pingpong(float in, final float length) {
         in %= length * 2;
         if (in < length) {
@@ -71,51 +71,51 @@ public strictfp class Mathf {
             return 2 * length - in;
         }
     }
-
+    
     public static float sin(final float rad) {
         return (float) Math.sin(rad);
     }
-
+    
     public static float cos(final float rad) {
         return (float) Math.cos(rad);
     }
-
+    
     public static float tan(final float rad) {
         return (float) Math.tan(rad);
     }
-
+    
     public static float arcsin(final float x) {
         return (float) Math.asin(x);
     }
-
+    
     public static float arccos(final float x) {
         return (float) Math.acos(x);
     }
-
+    
     public static float arctan(final float x) {
         return (float) java.lang.Math.atan(x);
     }
-
+    
     public static float arctan2(final float y, final float x) {
         return (float) Math.atan2(y, x);
     }
-
+    
     public static float sqrt(final float value) {
         return (float) Math.sqrt(value);
     }
-
+    
     public static float abs(final float value) {
         return value < 0.0f ? 0.0f - value : value;
     }
-
+    
     public static float min(final float v0, final float v1) {
         return v0 < v1 ? v0 : v1;
     }
-
+    
     public static float max(final float v0, final float v1) {
         return v0 > v1 ? v0 : v1;
     }
-
+    
     public static float floor(final float value) {
         if (value != value) {
             // NaN
@@ -130,7 +130,11 @@ public strictfp class Mathf {
         }
         return intvalue;
     }
-
+    
+    public static int floori(float value) {
+        return (int) floor(value);
+    }
+    
     public static float ceil(final float value) {
         if (value != value) {
             // NaN
@@ -145,7 +149,11 @@ public strictfp class Mathf {
         }
         return intvalue;
     }
-
+    
+    public static int ceili(float value) {
+        return (int) ceil(value);
+    }
+    
     public static float rint(final float value) {
         if (value != value) {
             // NaN
@@ -158,17 +166,17 @@ public strictfp class Mathf {
         }
         return value;
     }
-
+    
     public static int round(final float value) {
         return (int) rint(value);
     }
-
+    
     public static long roundl(final float value) {
         return (long) rint(value);
     }
-
+    
     public static float toRadians(final float i) {
         return (float) Math.toRadians(i);
     }
-
+    
 }
